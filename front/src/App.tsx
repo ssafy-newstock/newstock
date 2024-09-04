da322b36508a4e08ddf7600ed6ff40a0cb7e6452
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-09-05 07:06:32 +0900

    setting:절대 경로 설정

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,7 +3,8 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/GlobalStyle';
 import { lightTheme, darkTheme } from './styles/theme';
 import { useThemeStore } from './store/themeStore';
-import ThemedButton from './components/ThemedButton';
+// import ThemedButton from './components/ThemedButton';
+import ThemedButton from '@components/ThemedButton';
 
 const App = () => {
   const { theme, toggleTheme } = useThemeStore();
