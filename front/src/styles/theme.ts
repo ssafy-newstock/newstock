27a4dfdb09ac6972f1345fced8bda4483a82cc78
이명욱 <muk525@icloud.com>
2024-09-06 16:06:32 +0900

    setting:theme 타입 설정

--- front/src/styles/theme.ts
+++ front/src/styles/theme.ts
@@ -1,15 +1,15 @@
-export const lightTheme = {
+import { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
   backgroundColor: '#ffffff',
   textColor: '#000000',
   buttonBackgroundColor: '#007bff',
   buttonTextColor: '#ffffff',
 };
 
-export const darkTheme = {
+export const darkTheme: DefaultTheme = {
   backgroundColor: '#000000',
   textColor: '#ffffff',
   buttonBackgroundColor: '#1e1e1e',
   buttonTextColor: '#ffffff',
 };
-
-export type ThemeType = typeof lightTheme;
