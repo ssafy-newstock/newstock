3f0165090953af65045fc06129317ff21df0882a
thswltjr11 <thswltjr11@gmail.com>
2024-09-07 18:55:53 +0900

    deploy: test server push

---
+++ test/src/main/java/com/ssafy/test/TestController.java
@@ -0,0 +1,19 @@
+package com.ssafy.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World2222222222222222222222";
+    }
+}
