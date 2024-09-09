1a3514d09d793ba92752a6ed8291fbae3bec7d9d
고민호 <koho10477@naver.com>
2024-09-09 15:42:47 +0900

    feat : #4 주식 찜 엔티티 생성

---
+++ back/stock/src/main/java/com/ssafy/stock/domain/entity/StocksFavorite.java
@@ -0,0 +1,25 @@
+package com.ssafy.stock.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "stocks_favorite")
+public class StocksFavorite extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "stock_favorite_id")
+    private Long id;
+
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Stocks stock;
+}
