a9a99c416e27f288e83e225ec3897a2d34a512b0
고민호 <koho10477@naver.com>
2024-09-09 15:42:32 +0900

    feat : #4 주식 종목 엔티티 생성

---
+++ back/stock/src/main/java/com/ssafy/stock/domain/entity/Stocks.java
@@ -0,0 +1,40 @@
+package com.ssafy.stock.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stocks extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "stock_id")
+    private Long id;
+
+    @Column(length = 6)
+    private String stockCode;
+
+    private String stockName;
+
+    private String stockMarket;
+
+    private String stockIndustry;
+
+    @OneToMany(mappedBy = "stock", cascade = CascadeType.REMOVE)
+    private List<StocksCandle> stocksCandles;
+
+    @OneToMany(mappedBy = "stock", cascade = CascadeType.REMOVE)
+    private List<StocksFavorite> stocksFavorites;
+
+    @OneToMany(mappedBy = "stock", cascade = CascadeType.REMOVE)
+    private List<StocksTransactions> stocksTransactions;
+
+    @OneToMany(mappedBy = "stock", cascade = CascadeType.REMOVE)
+    private List<StocksHoldings> stocksHoldings;
+}
