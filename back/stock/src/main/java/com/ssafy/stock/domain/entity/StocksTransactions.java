a946d7eee251bcf1e59ce774d793b1c78260a102
고민호 <koho10477@naver.com>
2024-09-09 15:43:01 +0900

    feat : #4 주식 거래 내역 엔티티 생성

---
+++ back/stock/src/main/java/com/ssafy/stock/domain/entity/StocksTransactions.java
@@ -0,0 +1,35 @@
+package com.ssafy.stock.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "stocks_transactions")
+public class StocksTransactions extends BaseEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "stock_transaction_id")
+    private Long id;
+
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stock_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private Stocks stock;
+
+    private Long stockTransactionAmount;
+
+    private Long stockTransactionPrice;
+
+    private TYPE stockTransactionType;
+
+    private LocalDateTime stockTransactionDate;
+}
