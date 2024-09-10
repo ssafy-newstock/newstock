2933ae2daacbb1e59ff4e477b11a603e4531dc6f
고민호 <koho10477@naver.com>
2024-09-10 16:25:31 +0900

    feat : #11 주식 서비스 및 주식 정보 갱신 메소드 추가

---
+++ back/stock/src/main/java/com/ssafy/stock/domain/service/StockService.java
@@ -0,0 +1,166 @@
+package com.ssafy.stock.domain.service;
+
+import com.ssafy.stock.domain.entity.Stocks;
+import com.ssafy.stock.domain.entity.StocksPriceRedis;
+import com.ssafy.stock.domain.entity.StocksRedis;
+import com.ssafy.stock.domain.repository.StocksPriceRedisRepository;
+import com.ssafy.stock.domain.repository.StocksRedisRepository;
+import com.ssafy.stock.domain.repository.StocksRepository;
+import com.ssafy.stock.domain.service.helper.StockConverter;
+import com.ssafy.stock.domain.service.response.StockPricesKisResponseDto;
+import com.ssafy.stock.domain.service.response.StockPricesOutputKisResponseDto;
+import com.ssafy.stock.domain.service.response.StockPricesResponseDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class StockService {
+
+    @Value("${ACCESS_TOKEN1}")
+    private String ACCESS_TOKEN1;
+
+    @Value("${ACCESS_TOKEN2}")
+    private String ACCESS_TOKEN2;
+
+    @Value("${APP_KEY1}")
+    private String APP_KEY1;
+
+    @Value("${APP_SECRET1}")
+    private String APP_SECRET1;
+
+    @Value("${APP_KEY2}")
+    private String APP_KEY2;
+
+    @Value("${APP_SECRET2}")
+    private String APP_SECRET2;
+
+    private static final int BATCH_SIZE = 20;
+
+    private final StocksRepository stocksRepository;
+    private final StocksRedisRepository stocksRedisRepository;
+    private final StocksPriceRedisRepository stocksPriceRedisRepository;
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+    private final StockConverter stockConverter;
+
+    // 종목 정보 조회 전략 : Redis 캐시메모리 사용
+    @Cacheable(cacheNames = "stocksInfo", cacheManager = "cacheManager")
+    public List<StocksRedis> getStocksInfo(){
+        List<StocksRedis> stocksRedis = stocksRedisRepository.findAll();
+
+        if(stocksRedis.isEmpty()){
+            List<Stocks> stocks = stocksRepository.findAll();
+
+            stocksRedis = stocks.stream()
+                    .map(dto -> new StocksRedis(dto.getStockCode(), dto.getStockName()))
+                    .collect(Collectors.toList());
+
+            stocksRedisRepository.saveAll(stocksRedis);
+        }
+        return stocksRedis;
+    }
+
+    /**
+     * 30초 단위 코스피 958개 정보 갱신
+     * 갱신 목록 : 주식 현재가, 전일 대비, 전일 대비율
+     */
+    @Scheduled(fixedDelay = 10000)
+    private void fetchStockPrices() {
+        List<StocksRedis> stocksInfo = getStocksInfo();
+        List<StocksRedis> stockCodes1 = stocksInfo.subList(0, stocksInfo.size() / 2);
+        List<StocksRedis> stockCodes2 = stocksInfo.subList(stocksInfo.size() / 2, stocksInfo.size());
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        CompletableFuture<List<StockPricesResponseDto>> future1 = CompletableFuture.supplyAsync(() -> getStockPrices(stockCodes1, ACCESS_TOKEN1, APP_KEY1, APP_SECRET1), executor);
+        CompletableFuture<List<StockPricesResponseDto>> future2 = CompletableFuture.supplyAsync(() -> getStockPrices(stockCodes2, ACCESS_TOKEN2, APP_KEY2, APP_SECRET2), executor);
+
+        try{
+            List<StockPricesResponseDto> allStockPrices = Stream.of(future1, future2)
+                    .map(CompletableFuture::join)  // 비동기 작업을 완료하고 결과를 가져옴
+                    .flatMap(List::stream)         // 두 리스트를 하나로 병합
+                    .collect(Collectors.toList()); // List로 수집
+
+            List<StocksPriceRedis> stocksPriceRedisList = stockConverter.convertToStocksPriceRedisList(allStockPrices);
+
+            stocksPriceRedisRepository.deleteAll();
+            stocksPriceRedisRepository.saveAll(stocksPriceRedisList);
+
+            log.info("스케줄러 : 주식 데이터 갱신 성공");
+            simpMessageSendingOperations.convertAndSend("/api/sub/stock/info", allStockPrices);
+        } catch (CompletionException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 한국투자증권 시세 갱신 메소드
+     * @param stocksInfo 종목코드, 종목이름
+     * @param accessToken
+     * @param appKey
+     * @param appSecret
+     * @return 코스피 958개의 종목코드, 종목이름, 현재가, 전일 대비, 전일 대비율
+     */
+    private List<StockPricesResponseDto> getStockPrices(List<StocksRedis> stocksInfo, String accessToken, String appKey, String appSecret) {
+        List<StockPricesResponseDto> stockPricesResponseDtos = new ArrayList<>();
+
+        int totalStockCodes = stocksInfo.size();
+        int totalBatches = (int) Math.ceil((double) totalStockCodes / BATCH_SIZE);
+
+        for (int batch = 0; batch < totalBatches; batch++) {
+            int startIdx = batch * BATCH_SIZE;
+            int endIdx = Math.min(startIdx + BATCH_SIZE, totalStockCodes);
+
+            List<StocksRedis> batchStockCodes = stocksInfo.subList(startIdx, endIdx);
+
+            for (StocksRedis stockInfo : batchStockCodes) {
+                try {
+                    ResponseEntity<StockPricesKisResponseDto> response = stockConverter.exchangeRestTemplate(accessToken, appKey, appSecret, stockInfo);
+                    StockPricesKisResponseDto stockApiResponse = response.getBody();
+                    if (stockApiResponse != null) {
+                        StockPricesOutputKisResponseDto stockOutput = stockApiResponse.getOutput();
+                        if (stockOutput != null) {
+                            StockPricesResponseDto stockPricesResponseDto = stockConverter.ConvertToStockPricesResponseDto(stockInfo, stockOutput);
+                            stockPricesResponseDtos.add(stockPricesResponseDto);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            waitNextKsiApi();
+        }
+        return stockPricesResponseDtos;
+    }
+
+    /**
+     * 한국투자증권 API 요청 대시 메소드
+     * 1초에 20회 제한으로 인해 대기 시간 필요
+     */
+    private static void waitNextKsiApi() {
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public Iterable<StocksPriceRedis> getStocksPriceRedis(){
+        return stocksPriceRedisRepository.findAll();
+    }
+}
