5cd591af20c43d817e4a153dbb9022fafc27290d
손지석 <thswltjr11@gmail.com>
2024-09-10 10:57:18 +0900

    feat: #12 OAuth 관련 엔티티 추가

---
+++ back/member/src/main/java/com/ssafy/member/entity/Provider.java
@@ -0,0 +1,5 @@
+package com.ssafy.member.entity;
+
+public enum Provider {
+    KAKAO, NAVER
+}
---
+++ back/member/src/main/java/com/ssafy/member/entity/dto/OAuthAttributesDto.java
@@ -0,0 +1,91 @@
+package com.ssafy.member.entity.dto;
+
+import com.ssafy.member.entity.Member;
+import lombok.Getter;
+
+import java.util.Map;
+
+/*
+    OAuth2 인증을 통해 얻은 사용자 정보를 저장하고 관리하기 위한 데이터 구조
+ */
+@Getter
+public class OAuthAttributesDto {
+    private final Map<String, Object> attributes;
+    private final String nameAttributeKey;
+    private final String memberName;
+    private final String memberProvider;
+    private final String memberProviderEmail;
+    private final String memberProfileImageUrl;
+
+    public OAuthAttributesDto(Map<String, Object> attributes,
+                              String nameAttributeKey,
+                              String memberName,
+                              String memberProvider,
+                              String memberProviderEmail,
+                              String memberProfileImageUrl) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.memberName = memberName;
+        this.memberProvider = memberProvider;
+        this.memberProviderEmail = memberProviderEmail;
+        this.memberProfileImageUrl = memberProfileImageUrl;
+    }
+
+    /*
+        OAuth가 여러 개이면 후에 분기하는 코드
+     */
+    public static OAuthAttributesDto of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if ("kakao".equals(registrationId)) {
+            return ofKakao("id", registrationId, attributes);
+        }
+
+        // TODO: 나중에 삭제
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    // TODO: 나중에 삭제
+    private static OAuthAttributesDto ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return new OAuthAttributesDto(
+                attributes,
+                userNameAttributeName,
+                (String) attributes.get("name"),
+                "google",
+                (String) attributes.get("email"),
+                (String) attributes.get("picture")
+        );
+    }
+
+    private static OAuthAttributesDto ofKakao(String userNameAttributeName, String registrationId, Map<String, Object> attributes) {
+        // 카카오 정보 가져옴
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+
+        // 프로필 정보 가져옴
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        // 개별 정보 가져옴
+        String nickname = (String) profile.get("nickname");
+        String profileImageUrl = (String) profile.get("profile_image_url");
+        boolean isDefaultImage = (boolean) profile.get("is_default_image");
+
+        String providerEmail = (String) kakaoAccount.get("email");
+
+        // 생성자를 통해 OAuthAttributesDto 객체 반환
+        return new OAuthAttributesDto(
+                attributes,
+                userNameAttributeName, // oauth주체자가 user을 주체하는 식별자(ex. kakao는 id를 씀)
+                nickname,
+                registrationId, // memberProvider(ex. kakao, google)
+                providerEmail,
+                isDefaultImage ? "https://sarrr.s3.ap-northeast-2.amazonaws.com/%EC%96%BC%EC%9D%8C__1_-removebg-preview.png" : profileImageUrl
+        );
+    }
+
+    public Member toEntity() {
+        return new Member(
+                memberName,
+                memberProvider,
+                memberProviderEmail,
+                memberProfileImageUrl
+        );
+    }
+}
