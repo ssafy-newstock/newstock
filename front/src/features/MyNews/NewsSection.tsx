f2f36d05f4035fc94a3ba769662001df4620a800
이주호 <zxxv1245@naver.com>
2024-09-10 13:19:01 +0900

    chore: #5 Navbar

---
+++ front/src/features/MyNews/NewsSection.tsx
@@ -0,0 +1,46 @@
+import CenterNewsCard from './CenterNewsCard';
+import {
+  CenterContentSection,
+  CenterContentSectionTitle,
+} from './StyledComponent';
+
+interface NewsSectionProps {
+  title: string;
+}
+
+interface CardData {
+  Title: string;
+  Media: string;
+  Date: string;
+  keyword?: string;
+}
+
+const cards: CardData[] = [
+  {
+    Title: '[청년 디지털 인재] “AI부터 ...',
+    Media: '청년일보',
+    Date: '2024.08.18',
+    keyword: '삼성전자',
+  },
+  {
+    Title: '[청년 디지털 인재] “AI부터 ...',
+    Media: '청년일보',
+    Date: '2024.08.18',
+    keyword: '삼성전자',
+  },
+  {
+    Title: '[청년 디지털 인재] “AI부터 ...',
+    Media: '청년일보',
+    Date: '2024.08.18',
+  },
+];
+const NewsSection: React.FC<NewsSectionProps> = ({ title }) => (
+  <CenterContentSection>
+    <CenterContentSectionTitle>{title}</CenterContentSectionTitle>
+    {cards.map((data, index) => (
+      <CenterNewsCard key={index} data={data} />
+    ))}
+  </CenterContentSection>
+);
+
+export default NewsSection;
---
+++ front/src/features/MyNews/ScrapSection.tsx
@@ -0,0 +1,45 @@
+import CenterCard from './CenterNewsCard';
+import CenterScrapCard from './CenterScrapCard';
+import {
+  CenterContentSection,
+  CenterContentSectionTitle,
+} from './StyledComponent';
+
+interface ScrapSectionProps {
+  title: string;
+}
+
+interface CardData {
+  Title: string;
+  NewsTitle: string;
+  Date: string;
+}
+
+const cards: CardData[] = [
+  {
+    Title: '나 이정준 첫 번째 스크랩',
+    NewsTitle: '[청년 디지털 인재] “AI부터 ...',
+    Date: '2024.08.18',
+  },
+  {
+    Title: '나 이주호 두 번째 스크랩',
+    NewsTitle: '[청년 디지털 인재] “AI부터 ...',
+    Date: '2024.08.18',
+  },
+  {
+    Title: '아 숙제하기 싫다.',
+    NewsTitle: '[청년 디지털 인재] “AI부터 ...',
+    Date: '2024.08.18',
+  },
+];
+
+const ScrapSection: React.FC<ScrapSectionProps> = ({ title }) => (
+  <CenterContentSection>
+    <CenterContentSectionTitle>{title}</CenterContentSectionTitle>
+    {cards.map((data, index) => (
+      <CenterScrapCard key={index} data={data} />
+    ))}
+  </CenterContentSection>
+);
+
+export default ScrapSection;
