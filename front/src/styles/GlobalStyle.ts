d9a2d291b2c9c526ed818a1b8254671586125ab0
이명욱 <muk525@icloud.com>
2024-09-11 16:19:30 +0900

    refactor: index.html에서 글꼴 import

--- front/src/styles/GlobalStyle.ts
+++ front/src/styles/GlobalStyle.ts
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
