284e6e284a1974ccef6094aac3acfaea700faf43
고민호 <koho10477@naver.com>
2024-09-12 16:18:39 +0900

    feat : #20 종목 산업군 Redis

---
+++ back/stock/src/main/java/com/ssafy/stock/domain/entity/StockIndustryRedis.java
@@ -0,0 +1,29 @@
+package com.ssafy.stock.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash(value = "StockIndustryRedis")
+public class StockIndustryRedis {
+    @Id
+    private String industryCode; // 업종 코드
+    private String industryName; // 업종 이름
+    private String bstpNmixPrpr;  // 업종 지수 현재가
+    private String bstpNmixPrdyVrss; // 업종 지수 전일 대비
+    private String bstpNmixPrdyCtrt; // 업종 지수 전일 대비율
+    private String acmlTrPbmn;    // 거래대금(백만)
+
+    @Override
+    public String toString() {
+        return String.format(
+                "StockSectorDto [업종 코드 =%s, 업종 이름 =%s, 현재 지수=%s, 전일대비=%s, 등락률=%s, 거래대금(백만)=%s]",
+                industryCode, industryName, bstpNmixPrpr, bstpNmixPrdyVrss, bstpNmixPrdyCtrt, acmlTrPbmn
+        );
+    }
+}
---
+++ back/stock/src/main/java/com/ssafy/stock/domain/repository/StockIndustryRedisRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.stock.domain.repository;
+
+import com.ssafy.stock.domain.entity.StockIndustryRedis;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockIndustryRedisRepository extends CrudRepository<StockIndustryRedis, String> {
+}
