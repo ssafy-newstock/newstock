12cc01d642d38ffe2cb1a18d106269976ec9a804
고민호 <koho10477@naver.com>
2024-09-12 16:17:52 +0900

    feat : #20 주식 컨트롤러 산업 카테고리, top10 종목 조회 추가

--- back/stock/src/main/java/com/ssafy/stock/domain/controller/StockController.java
+++ back/stock/src/main/java/com/ssafy/stock/domain/controller/StockController.java
@@ -1,6 +1,9 @@
 package com.ssafy.stock.domain.controller;
 
+import com.ssafy.stock.domain.entity.StockIndustryRedis;
+import com.ssafy.stock.domain.entity.StocksPriceLiveRedis;
 import com.ssafy.stock.domain.entity.StocksPriceRedis;
+import com.ssafy.stock.domain.service.StockIndustryService;
 import com.ssafy.stock.domain.service.StockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,11 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class StockController {
     private final StockService stockService;
+    private final StockIndustryService stockIndustryService;
 
     @GetMapping("/price-list")
     public ResponseEntity<?> getStockPriceList(){
         Iterable<StocksPriceRedis> stocksPriceRedis = stockService.getStocksPriceRedis();
         return ResponseEntity.ok()
                 .body(stocksPriceRedis);
+    }
+
+    @GetMapping("/price-list/live")
+    public ResponseEntity<?> getStockPriceLiveList(){
+        Iterable<StocksPriceLiveRedis> stocksPriceLiveRedis = stockService.getStocksPriceLiveRedis();
+        return ResponseEntity.ok()
+                .body(stocksPriceLiveRedis);
+    }
+
+    @GetMapping("/industry-list")
+    public ResponseEntity<?> getStockIndustryList(){
+        Iterable<StockIndustryRedis> stockIndustryRedisList = stockIndustryService.getStockIndustryRedisList();
+        return ResponseEntity.ok()
+                .body(stockIndustryRedisList);
     }
 }
