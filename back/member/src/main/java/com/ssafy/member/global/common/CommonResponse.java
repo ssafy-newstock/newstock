abebbe2b39f7764aeaa49fc91e78cce20e24a0e1
thswltjr11 <thswltjr11@gmail.com>
2024-09-15 16:15:35 +0900

    refactor: #38 불필요한 Data 어노테이션 삭제

--- back/member/src/main/java/com/ssafy/member/global/common/CommonResponse.java
+++ back/member/src/main/java/com/ssafy/member/global/common/CommonResponse.java
@@ -2,9 +2,8 @@ package com.ssafy.member.global.common;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@Data
+
 @AllArgsConstructor
 // 이게 있으면 null data 는 보내지지 않음
 @JsonInclude(JsonInclude.Include.NON_NULL)
