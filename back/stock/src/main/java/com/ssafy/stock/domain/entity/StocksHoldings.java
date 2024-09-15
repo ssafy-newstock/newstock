0acf93e3d0659af83a1b3f242f74664b56b61272
고민호 <koho10477@naver.com>
2024-09-15 17:11:39 +0900

    refactor : #39 보유 주식 생성자 및 update 메소드 추가

--- back/stock/src/main/java/com/ssafy/stock/domain/entity/StocksHoldings.java
+++ back/stock/src/main/java/com/ssafy/stock/domain/entity/StocksHoldings.java
@@ -26,4 +26,22 @@ public class StocksHoldings extends BaseEntity{
     private Long stockHoldingBuyAmount;
 
     private Long stockHoldingBuyPrice;
+
+    public StocksHoldings(Long memberId, Stocks stock, Long stockHoldingBuyAmount, Long stockHoldingBuyPrice) {
+        this.memberId = memberId;
+        this.stock = stock;
+        this.stockHoldingBuyAmount = stockHoldingBuyAmount;
+        this.stockHoldingBuyPrice = stockHoldingBuyPrice;
+    }
+
+    public void update(Long amount, Long price, TYPE updateType){
+        if(updateType == TYPE.BUY){
+            this.stockHoldingBuyAmount += amount;
+            this.stockHoldingBuyPrice += price;
+        } else if(updateType == TYPE.SELL){
+            this.stockHoldingBuyAmount -= amount;
+            this.stockHoldingBuyPrice -= price;
+        }
+
+    }
 }
