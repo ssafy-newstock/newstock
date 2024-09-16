64b91c7372704a9ffb6fb14ecf866c9b9dc77d68
thswltjr11 <thswltjr11@gmail.com>
2024-09-16 18:43:40 +0900

    feat: #41 buy, sell API 구현

--- back/member/src/main/java/com/ssafy/member/domain/controller/MemberController.java
+++ back/member/src/main/java/com/ssafy/member/domain/controller/MemberController.java
@@ -1,7 +1,8 @@
 package com.ssafy.member.domain.controller;
 
 import com.ssafy.member.domain.controller.request.MemberPointUpdateRequest;
-import com.ssafy.member.domain.controller.response.MemberGetPointResponse;
+import com.ssafy.member.domain.controller.response.MemberPointResponse;
+import com.ssafy.member.domain.entity.Member;
 import com.ssafy.member.domain.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,27 +21,44 @@ public class MemberController {
      * @param memberId 멤버 아이디
      * @return 200, MemberGetPointResponse
      */
-    @GetMapping("/point/{memberId}")
+    @GetMapping("{memberId}/point")
     public ResponseEntity<?> myPoint(@PathVariable Long memberId) {
         Long myPoint = memberService.getMyPoint(memberId);
 
-        MemberGetPointResponse response = new MemberGetPointResponse(memberId, myPoint);
+        MemberPointResponse response = new MemberPointResponse(memberId, myPoint);
         return ResponseEntity.ok(response);
     }
 
+    /**
+     * 주식을 매수하였을 때 포인트를 업데이트하는 API
+     * @param memberId 멤버 아이디
+     * @param request MemberPointUpdateRequest
+     * @return MemberPointResponse
+     */
+    @PostMapping("{memberId}/buy")
+    public ResponseEntity<?> buyStock(
+            @PathVariable Long memberId,
+            @RequestBody MemberPointUpdateRequest request) {
+        Member member = memberService.buyStock(memberId, request.getPoint());
+        MemberPointResponse response = new MemberPointResponse(memberId, member.getPoint());
+        return ResponseEntity
+                .ok(response);
+    }
 
     /**
-     * 해당 멤버가 가진 포인트를 업데이트 할 수 있는 컨트롤러
-     *
-     * @param request
-     * @return 204, no-content
+     * 주식이 매도되었을 때, 포인트를 업데이트 하는 API
+     * @param memberId 멤버 아이디
+     * @param request MemberPointUpdateRequest
+     * @return MemberPointResponse
      */
-    @PutMapping("/point/{memberId}")
-    public ResponseEntity<?> updateMyPoint(@PathVariable Long memberId, @RequestBody MemberPointUpdateRequest request) {
-        memberService.updateMyPoint(memberId, request.getPoint());
+    @PostMapping("{memberId}/sell")
+    public ResponseEntity<?> sellStock(
+            @PathVariable Long memberId,
+            @RequestBody MemberPointUpdateRequest request) {
+        Member member = memberService.sellStock(memberId, request.getPoint());
+        MemberPointResponse response = new MemberPointResponse(memberId, member.getPoint());
         return ResponseEntity
-                .noContent()
-                .build();
+                .ok(response);
     }
 
 
