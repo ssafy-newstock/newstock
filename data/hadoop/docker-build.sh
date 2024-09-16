c7b528926e0608c256df3fa1eb1ccf1628b13663
chestnut1717 <psh56750855@gmail.com>
2024-09-16 21:58:29 +0900

    feat: docker-compose 및 build bash 파일 추가

---
+++ data/hadoop/docker-build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+docker-compose down
+
+docker build -t language-base ./language-base
+docker build -t hadoop-base ./hadoop-base
+docker build -t hadoop-spark-base ./hadoop-spark-base
+
+docker build -t hadoop-namenode ./namenode
+docker build -t hadoop-datanode ./datanode
+docker build -t resourcemanager ./resourcemanager
+docker build -t yarn-timelineserver ./yarntimelineserver
+docker build -t spark-historyserver ./sparkhistoryserver
+# docker build -t zeppelin ./hadoop/zeppelin
+
+docker-compose up -d
