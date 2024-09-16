5ad0ff7d8d778da6467fdca4fe1c52cfae8fbb8a
고민호 <koho10477@naver.com>
2024-09-16 22:05:24 +0900

    feat : #39 member api 요청을 위한 MemberFeignClient 구현

---
+++ back/stock/src/main/java/com/ssafy/stock/global/util/MemberFeignClient.java
@@ -0,0 +1,27 @@
+package com.ssafy.stock.global.util;
+
+import com.ssafy.stock.domain.service.request.MemberPointUpdateRequest;
+import com.ssafy.stock.global.common.MemberIdRequest;
+import com.ssafy.stock.global.common.MemberIdResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "memberClient", url = "http://newstock.info/api/member")
+public interface MemberFeignClient {
+
+    @PostMapping("/{memberId}/buy")
+    ResponseEntity<?> updateBuyingPoints(
+            @PathVariable("memberId") Long memberId,
+            @RequestBody MemberPointUpdateRequest pointRequestDto);
+
+    @PostMapping("/{memberId}/sell")
+    ResponseEntity<?> updateSellingPoints(
+            @PathVariable("memberId") Long memberId,
+            @RequestBody MemberPointUpdateRequest pointRequestDto);
+
+    @PostMapping("/verify")
+    ResponseEntity<MemberIdResponse> getMemberId(@RequestBody MemberIdRequest memberIdRequest);
+}
