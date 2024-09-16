7d1066634686a788d5d59ae97e0645465eda390a
고민호 <koho10477@naver.com>
2024-09-16 22:06:59 +0900

    feat : #39 StockHoldingNotFoundException custom exception 추가

--- back/stock/src/main/java/com/ssafy/stock/domain/error/custom/StockHoldingNotFoundException.java
+++ back/stock/src/main/java/com/ssafy/stock/domain/error/custom/StockHoldingNotFoundException.java
@@ -2,7 +2,7 @@ package com.ssafy.stock.domain.error.custom;
 
 public class StockHoldingNotFoundException extends RuntimeException {
     public StockHoldingNotFoundException() {
-        super("해당 종목을 보유하고 있지 않습니다.");
+        super("이 주식은 현재 보유하고 있지 않습니다. 보유 목록을 확인해 주세요.");
     }
 
     public StockHoldingNotFoundException(String message) {
