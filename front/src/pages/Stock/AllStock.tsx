884f63c7e6550193a3f68e24edf52d7dfdb79e50
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-09-17 23:42:26 +0900

    chore: import react 삭제

--- front/src/pages/Stock/AllStock.tsx
+++ front/src/pages/Stock/AllStock.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { Center } from '@components/Center';
 import LeftStock from '@components/LeftStock';
 import AllStock, { AllStockFirstRow } from '@features/Stock/AllStock/AllStock';
