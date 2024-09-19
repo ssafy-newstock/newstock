8c10071beed238e6b84137c1e0a1797c7660cb90
이명욱 <muk525@icloud.com>
2024-09-19 10:20:47 +0900

    feat: 웹소켓 연결

--- front/src/pages/Stock/StockMain.tsx
+++ front/src/pages/Stock/StockMain.tsx
@@ -1,7 +1,7 @@
-// import { useEffect, useState } from 'react';
-// import SockJS from 'sockjs-client';
-// import Stomp from 'stompjs';
-import { useQuery, } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { Center } from '@components/Center';
 import LeftStock from '@components/LeftStock';
@@ -24,7 +24,6 @@ import { ICategoryStock, IStock } from '@features/Stock/types';
 import { RightVacant } from '@components/RightVacant';
 import { stockData } from '@features/Stock/stock';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
 import Modal from '@features/Stock/SectionStock/Modal';
 
 const StockMainPage = () => {
@@ -36,19 +35,62 @@ const StockMainPage = () => {
     navigate('/section-stock');
   };
 
-    // 모달 관련
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [selectedCategory, setSelectedCategory] = useState<ICategoryStock | null>(null);
-  
-    const openModal = (category: ICategoryStock) => {
-      setSelectedCategory(category);
-      setIsModalOpen(true);
+  // 모달 관련
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] =
+    useState<ICategoryStock | null>(null);
+
+  const openModal = (category: ICategoryStock) => {
+    setSelectedCategory(category);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setSelectedCategory(null);
+    setIsModalOpen(false);
+  };
+
+  const queryClient = useQueryClient();
+  // const [isWebSocketConnected, setIsWebSocketConnected] = useState(false);
+
+  // 웹소켓 초기화 및 구독 설정
+  useEffect(() => {
+    const socket = new SockJS('http://newstock.info/api/stock/websocket');
+    const stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, () => {
+      // setIsWebSocketConnected(true);
+      // Top 10 종목 정보 구독
+      stompClient.subscribe('/api/sub/stock/info/live', (message) => {
+        const updatedStockData = JSON.parse(message.body);
+        console.log("updatedStockData", updatedStockData);
+        queryClient.setQueryData(['top10StockData'], updatedStockData);
+      });
+
+      // 산업군 정보 구독 (10분 단위 갱신)
+      stompClient.subscribe('/api/sub/stock/industry/info', (message) => {
+        const updatedIndustryData = JSON.parse(message.body);
+        console.log("updatedIndustryData", updatedIndustryData);
+        queryClient.setQueryData(['industryData'], updatedIndustryData);
+      });
+
+      // 코스피 전 종목 정보 구독 (30~40초 단위 갱신)
+      stompClient.subscribe('/api/sub/stock/info', (message) => {
+        const updatedStockData = JSON.parse(message.body);
+        console.log("updatedStockData", updatedStockData);
+        queryClient.setQueryData(['allStockData'], updatedStockData);
+      });
+    });
+
+    return () => {
+      if (stompClient && stompClient.connected) {
+        stompClient.disconnect(() => {
+          console.log('WebSocket disconnected');
+          // setIsWebSocketConnected(false); // 연결 상태를 업데이트
+        });
+      }
     };
-  
-    const closeModal = () => {
-      setSelectedCategory(null);
-      setIsModalOpen(false);
-    };
+  }, [queryClient]);
 
   // 최초 데이터 조회 - axios 사용
   const { data: top10StockData, isLoading: isTop10StockLoading } = useQuery({
@@ -103,7 +145,7 @@ const StockMainPage = () => {
         </StockGridColumn>
 
         <StockHeaderMore>실시간 차트</StockHeaderMore>
-        <More onClick={allStockNavigate}/>
+        <More onClick={allStockNavigate} />
         <HrTag />
         <StockGridRow>
           <RealTimeStockFirstRow />
@@ -113,32 +155,34 @@ const StockMainPage = () => {
         </StockGridRow>
 
         <StockHeaderMore>카테고리</StockHeaderMore>
-        <More onClick={categoryNavigate}/>
+        <More onClick={categoryNavigate} />
         <HrTag />
         <CategoryGridColumn>
-          {industryData?.data.slice(0,3).map((category: ICategoryStock, index: number) => {
-            // 기본 이미지 객체
-            const defaultImage = {
-              url: 'default-image-url',
-              backgroundColor: 'default-bg-color',
-            };
-            // 카테고리 이미지 객체를 찾고, 없으면 기본 이미지 사용
-            const imageUrl =
-              category.industryName in categoryImage
-                ? categoryImage[
-                    category.industryName as keyof typeof categoryImage
-                  ]
-                : defaultImage; // 기본 이미지 객체로 처리
-            return (
-              <CategoryStock
-                key={index}
-                category={category}
-                imageUrl={imageUrl.url}
-                imageBgColor={imageUrl.backgroundColor}
-                onClick={() => openModal(category)}
-              />
-            );
-          })}
+          {industryData?.data
+            .slice(0, 3)
+            .map((category: ICategoryStock, index: number) => {
+              // 기본 이미지 객체
+              const defaultImage = {
+                url: 'default-image-url',
+                backgroundColor: 'default-bg-color',
+              };
+              // 카테고리 이미지 객체를 찾고, 없으면 기본 이미지 사용
+              const imageUrl =
+                category.industryName in categoryImage
+                  ? categoryImage[
+                      category.industryName as keyof typeof categoryImage
+                    ]
+                  : defaultImage; // 기본 이미지 객체로 처리
+              return (
+                <CategoryStock
+                  key={index}
+                  category={category}
+                  imageUrl={imageUrl.url}
+                  imageBgColor={imageUrl.backgroundColor}
+                  onClick={() => openModal(category)}
+                />
+              );
+            })}
         </CategoryGridColumn>
       </Center>
       <RightVacant />
@@ -150,47 +194,3 @@ const StockMainPage = () => {
 };
 
 export default StockMainPage;
-
-  // const queryClient = useQueryClient();
-  // const [isWebSocketConnected, setIsWebSocketConnected] = useState(false);
-
-  // // 웹소켓 초기화 및 구독 설정
-  // useEffect(() => {
-  //   const socket = new SockJS('http://newstock.info/api/stock/websocket');
-  //   const stompClient = Stomp.over(socket);
-
-  //   stompClient.connect({}, () => {
-  //     setIsWebSocketConnected(true);
-  //     // Top 10 종목 정보 구독
-  //     stompClient.subscribe('/sub/stock/info/live', (message) => {
-  //       const updatedStockData = JSON.parse(message.body);
-  //       queryClient.setQueryData(
-  //         { queryKey: ['top10StockData'] },
-  //         updatedStockData
-  //       );
-  //     });
-
-  //     // 산업군 정보 구독 (10분 단위 갱신)
-  //     stompClient.subscribe('/sub/stock/industry/info', (message) => {
-  //       const updatedIndustryData = JSON.parse(message.body);
-  //       queryClient.setQueryData(
-  //         { queryKey: ['industryData'] },
-  //         updatedIndustryData
-  //       );
-  //     });
-
-  //     // 코스피 전 종목 정보 구독 (30~40초 단위 갱신)
-  //     stompClient.subscribe('/sub/stock/info', (message) => {
-  //       const updatedStockData = JSON.parse(message.body);
-  //       queryClient.setQueryData(
-  //         { queryKey: ['allStockData'] },
-  //         updatedStockData
-  //       );
-  //     });
-  //   });
-
-  //   return () => {
-  //     stompClient.disconnect();
-  //     setIsWebSocketConnected(false); // 연결 해제
-  //   };
-  // }, [queryClient]);
