fea504051d63c7b750390f1c2da7143566ce6b2d
이명욱 <muk525@icloud.com>
2024-09-19 13:55:37 +0900

    feat: 숫자 변환 함수(조 단위 이상일 때의 케이스 추가)

--- front/src/utils/formatUnit.ts
+++ front/src/utils/formatUnit.ts
@@ -2,7 +2,16 @@ export const formatUnit = (value: string | number): string => {
   // string일 경우 숫자로 변환
   const numericValue = typeof value === 'string' ? Number(value.replace(/,/g, '')) : value;
 
-  if (numericValue >= 100000000) {
+  if (numericValue >= 1000000000000) {
+    const trillion = Math.floor(numericValue / 1000000000000);
+    const billion = Math.floor((numericValue % 1000000000000) / 1000000000);
+    if (billion > 0) {
+      return `${trillion}조 ${billion}천억`;
+    }
+    return `${trillion}조`;
+  } else if (numericValue >= 100000000000) {
+    return `${Math.floor(numericValue / 100000000000)}천억`; // 1천억 이상일 때 "천억"
+  } else if (numericValue >= 100000000) {
     return `${Math.floor(numericValue / 100000000)}억`; // 1억 이상일 때 "억"
   } else if (numericValue >= 10000000) {
     return `${Math.floor(numericValue / 10000000)}천만`; // 1천만 이상일 때 "천만"
