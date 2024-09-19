066020af93618a41e50eb4038b78e8b28a54df67
이주호 <zxxv1245@naver.com>
2024-09-19 09:18:09 +0900

    feat: #45 ScrapCreate 구현

---
+++ front/src/features/Scrap/create/CenterTitle.tsx
@@ -0,0 +1,11 @@
+import { TitleDiv, TitleP } from '../scrapStyledComponent';
+
+const CenterTitle: React.FC = () => {
+  return (
+    <TitleDiv>
+      <TitleP>스크랩 작성</TitleP>
+    </TitleDiv>
+  );
+};
+
+export default CenterTitle;
--- front/src/pages/Etc/ScrapCreate.tsx
+++ front/src/pages/Etc/ScrapCreate.tsx
@@ -1,13 +1,31 @@
-import { Center } from '@components/Center';
 import LeftNews from '@components/LeftNews';
+import { Center } from '@components/Center';
 import { Right } from '@components/Right';
+import CenterContent from '@features/Scrap/create/CenterContent';
+import CenterTitle from '@features/Scrap/create/CenterTitle';
+import RightContent from '@features/Scrap/create/RightContent';
+import RightTitle from '@features/Scrap/create/RightTitle';
+import { RightDiv, ScrapHr } from '@features/Scrap/scrapStyledComponent';
+import { CenterDiv } from '@features/MyNews/styledComponent';
 
 const ScrapCreatePage = () => {
   return (
     <>
       <LeftNews />
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <CenterDiv>
+          <CenterTitle />
+          <ScrapHr />
+          <CenterContent />
+        </CenterDiv>
+      </Center>
+      <Right>
+        <RightDiv>
+          <RightTitle />
+          <ScrapHr />
+          <RightContent />
+        </RightDiv>
+      </Right>
     </>
   );
 };
