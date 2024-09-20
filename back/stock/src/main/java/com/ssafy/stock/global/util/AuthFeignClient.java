eebc9b7c8baec76623e32151bb76fce154dd9b56
손지석 <thswltjr11@gmail.com>
2024-09-20 09:24:05 +0900

    fix: #42 feign client 헤더 추가

--- back/stock/src/main/java/com/ssafy/stock/global/util/AuthFeignClient.java
+++ back/stock/src/main/java/com/ssafy/stock/global/util/AuthFeignClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "authClient", url = "http://newstock-stock-auth-service:8000/api/auth")
 public interface AuthFeignClient {
-    @PostMapping("/token-info")
+    @PostMapping(value = "/token-info", consumes = "application/json", produces = "application/json")
     MemberIdResponse getMemberId(@RequestBody MemberIdRequest memberIdRequest);
 }
