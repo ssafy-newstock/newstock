7815f77a6cc7a1230b5a29e07cb73db85b67c6ba
손지석 <thswltjr11@gmail.com>
2024-09-20 09:08:04 +0900

    fix: #42 Feign client 포트번호 명시

--- back/stock/src/main/java/com/ssafy/stock/global/util/AuthFeignClient.java
+++ back/stock/src/main/java/com/ssafy/stock/global/util/AuthFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "authClient", url = "http://newstock-stock-auth-service/api/auth")
+@FeignClient(name = "authClient", url = "http://newstock-stock-auth-service:8000/api/auth")
 public interface AuthFeignClient {
     @PostMapping("/token-info")
     MemberIdResponse getMemberId(@RequestBody MemberIdRequest memberIdRequest);
