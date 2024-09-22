214c8275ca3e0e22a679993747a67814535142a1
고민호 <koho10477@naver.com>
2024-09-22 13:41:59 +0900

    feat : #52 일봉데이터 응답 객체 생성

---
+++ back/stock/src/main/java/com/ssafy/stock/domain/service/response/StockCandleDto.java
@@ -0,0 +1,33 @@
+package com.ssafy.stock.domain.service.response;
+
+import com.ssafy.stock.domain.entity.Stocks;
+import com.ssafy.stock.domain.entity.StocksCandle;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockCandleDto { ;
+    private Long stockId;
+    private String stockCode;
+    private Long stockCandleId;
+    private LocalDateTime stockCandleDay;
+    private Long stockCandleOpen;
+    private Long stockCandleClose;
+    private Long stockCandleHigh;
+    private Long stockCandleLow;
+
+    public StockCandleDto(Stocks stock, StocksCandle stockCandle) {
+        this.stockId = stock.getId();
+        this.stockCode = stock.getStockCode();
+        this.stockCandleId = stockCandle.getId();
+        this.stockCandleOpen = stockCandle.getStockCandleOpen();
+        this.stockCandleClose = stockCandle.getStockCandleClose();
+        this.stockCandleHigh = stockCandle.getStockCandleHigh();
+        this.stockCandleLow = stockCandle.getStockCandleLow();
+    }
+}
