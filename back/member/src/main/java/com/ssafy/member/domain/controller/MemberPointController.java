d63583b9a5b5a2f4bcef7bd839e3ac030c550ec9
손지석 <thswltjr11@gmail.com>
2024-09-24 15:02:47 +0900

    feat: #61 최초 포인트 반환 로직 구현

---
+++ back/member/src/main/java/com/ssafy/member/domain/controller/MemberPointController.java
@@ -0,0 +1,33 @@
+package com.ssafy.member.domain.controller;
+
+import com.ssafy.member.domain.controller.request.MemberPointRequest;
+import com.ssafy.member.domain.entity.Member;
+import com.ssafy.member.domain.repository.MemberRepository;
+import com.ssafy.member.global.exception.MemberNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberPointController {
+    private final SimpMessageSendingOperations messagingTemplate;
+    private final MemberRepository memberRepository;
+
+    @MessageMapping("/member/info/point")
+    @SendTo("/api/sub/member/info/point")
+    public Long sendMemberPoint(MemberPointRequest request) {
+        // request에서 memberId로 포인트 조회
+        Long memberId = request.getMemberId();
+        Long point = findMemberPointById(memberId); // 포인트 조회 로직
+        return point;  // 클라이언트에게 포인트 정보 전송
+    }
+
+    private Long findMemberPointById(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new MemberNotFoundException(memberId));
+        return member.getPoint();
+    }
+}
+
