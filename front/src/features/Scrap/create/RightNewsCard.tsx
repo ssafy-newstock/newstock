be7a5238afd4cc9b0eb26bf6f413504734905d58
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-09-25 20:23:54 +0900

    fix: 빌드 에러 주석 처리

--- front/src/features/Scrap/create/RightNewsCard.tsx
+++ front/src/features/Scrap/create/RightNewsCard.tsx
@@ -1,54 +1,54 @@
-import { TextP_16, TextP_22 } from '@features/Scrap/scrapStyledComponent';
-import {
-  RightNewsCardBottomDiv,
-  RightNewsCardDiv,
-  RightNewsCardTagDiv,
-  RightNewsCardTagP,
-} from '@features/Scrap/create/scrapCreateRightStyledComponent';
+// import { TextP_16, TextP_22 } from '@features/Scrap/scrapStyledComponent';
+// import {
+//   RightNewsCardBottomDiv,
+//   RightNewsCardDiv,
+//   RightNewsCardTagDiv,
+//   RightNewsCardTagP,
+// } from '@features/Scrap/create/scrapCreateRightStyledComponent';
 
-interface Data {
-  title: string;
-  media: string;
-  uploadDatetime: string;
-  description: string;
-  keywords?: string[];
-}
+// interface Data {
+//   title: string;
+//   media: string;
+//   uploadDatetime: string;
+//   description: string;
+//   keywords?: string[];
+// }
 
-interface RightNewsProps {
-  data: Data;
-}
+// interface RightNewsProps {
+//   data: Data;
+// }
 
-const RightNewsCard: React.FC<RightNewsProps> = ({ data }) => {
-  const handleDragStart = (e: React.DragEvent) => {
-    e.dataTransfer.setData('text/plain', JSON.stringify(data));
-    e.dataTransfer.effectAllowed = 'move'; // 이동 가능한 요소로 설정
-    document.body.style.cursor = 'grabbing'; // 드래그 시작 시 커서 변경
-  };
+// const RightNewsCard: React.FC<RightNewsProps> = ({ data }) => {
+//   const handleDragStart = (e: React.DragEvent) => {
+//     e.dataTransfer.setData('text/plain', JSON.stringify(data));
+//     e.dataTransfer.effectAllowed = 'move'; // 이동 가능한 요소로 설정
+//     document.body.style.cursor = 'grabbing'; // 드래그 시작 시 커서 변경
+//   };
 
-  const handleDragEnd = () => {
-    document.body.style.cursor = 'default'; // 드래그 끝난 후 커서 원래대로
-  };
+//   const handleDragEnd = () => {
+//     document.body.style.cursor = 'default'; // 드래그 끝난 후 커서 원래대로
+//   };
 
-  return (
-    <RightNewsCardDiv
-      draggable
-      onDragStart={handleDragStart}
-      onDragEnd={handleDragEnd} // 드래그 끝난 후 커서 복구
-      style={{ cursor: 'grab' }} // 기본적으로 'grab' 커서
-    >
-      <TextP_22>{data.title}</TextP_22>
-      <RightNewsCardBottomDiv>
-        <TextP_16>
-          {data.media} | {data.uploadDatetime}
-        </TextP_16>
-        {data.keywords && (
-          <RightNewsCardTagDiv>
-            <RightNewsCardTagP>{data.keywords[0]}</RightNewsCardTagP>
-          </RightNewsCardTagDiv>
-        )}
-      </RightNewsCardBottomDiv>
-    </RightNewsCardDiv>
-  );
-};
+//   return (
+//     <RightNewsCardDiv
+//       draggable
+//       onDragStart={handleDragStart}
+//       onDragEnd={handleDragEnd} // 드래그 끝난 후 커서 복구
+//       style={{ cursor: 'grab' }} // 기본적으로 'grab' 커서
+//     >
+//       <TextP_22>{data.title}</TextP_22>
+//       <RightNewsCardBottomDiv>
+//         <TextP_16>
+//           {data.media} | {data.uploadDatetime}
+//         </TextP_16>
+//         {data.keywords && (
+//           <RightNewsCardTagDiv>
+//             <RightNewsCardTagP>{data.keywords[0]}</RightNewsCardTagP>
+//           </RightNewsCardTagDiv>
+//         )}
+//       </RightNewsCardBottomDiv>
+//     </RightNewsCardDiv>
+//   );
+// };
 
-export default RightNewsCard;
+// export default RightNewsCard;
--- front/src/pages/Etc/ScrapCreate.tsx
+++ front/src/pages/Etc/ScrapCreate.tsx
@@ -2,7 +2,7 @@ import LeftNews from '@components/LeftNews';
 import { Center } from '@components/Center';
 import { Right } from '@components/Right';
 import CenterContent from '@features/Scrap/create/CenterContent';
-import RightContent from '@features/Scrap/create/RightContent';
+// import RightContent from '@features/Scrap/create/RightContent';
 import RightTitle from '@features/Scrap/create/RightTitle';
 import { RightDiv, ScrapHr } from '@features/Scrap/scrapStyledComponent';
 import { CenterDiv } from '@features/MyNews/styledComponent';
@@ -20,7 +20,7 @@ const ScrapCreatePage = () => {
         <RightDiv>
           <RightTitle />
           <ScrapHr />
-          <RightContent />
+          {/* <RightContent /> */}
         </RightDiv>
       </Right>
     </>
