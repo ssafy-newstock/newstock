4d023b6ba5ed2b1aaf39249d1664d1adfffa6f3a
이주호 <zxxv1245@naver.com>
2024-09-25 15:18:36 +0900

    chore: #53 스크랩 섹션 삭제

--- front/src/features/MyNews/CenterContent.tsx
+++ front/src/features/MyNews/CenterContent.tsx
@@ -1,13 +1,11 @@
-import NewsSection from './NewsSection';
-import ScrapSection from './ScrapSection';
-import { CenterContentDiv } from './styledComponent';
+import NewsSection from '@features/MyNews/NewsSection';
+import { CenterContentDiv } from '@features/MyNews/styledComponent';
 
 const CenterContent: React.FC = () => {
   return (
     <CenterContentDiv>
       <NewsSection title={'시황 뉴스'} />
       <NewsSection title={'종목 뉴스'} />
-      <ScrapSection title={'스크랩'} />
     </CenterContentDiv>
   );
 };
