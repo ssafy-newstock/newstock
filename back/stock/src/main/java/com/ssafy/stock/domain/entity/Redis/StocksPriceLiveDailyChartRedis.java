13adaaff686a18977a1b6ca1045d4abc405d3243
고민호 <koho10477@naver.com>
2024-09-25 11:03:58 +0900

    feat : #69 TOP10 주식 데일리 차트 Redis 객체

---
+++ back/stock/src/main/java/com/ssafy/stock/domain/entity/Redis/StocksPriceLiveDailyChartRedis.java
@@ -0,0 +1,37 @@
+package com.ssafy.stock.domain.entity.Redis;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash(value = "StocksPriceLiveDailyChartRedis")
+public class StocksPriceLiveDailyChartRedis {
+    @Id
+    private String id;
+
+    @Indexed
+    private String stockCode;     // 종목 코드
+    private String stockName;     // 종목 이름
+    private String stockIndustry; // 종목 카테고리
+    private Long stckPrpr;      // 주식 현재가
+    private LocalDateTime time;
+
+    public StocksPriceLiveDailyChartRedis(String stockCode, String stockName, String stockIndustry, Long stckPrpr, LocalDateTime time) {
+        this.id = stockCode + "|" + time;
+        this.stockCode = stockCode;
+        this.stockName = stockName;
+        this.stockIndustry = stockIndustry;
+        this.stckPrpr = stckPrpr;
+        this.time = time;
+    }
+}
