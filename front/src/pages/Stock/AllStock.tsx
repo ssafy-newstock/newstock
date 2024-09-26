1e71d17faece56e7a5f5cf24cea618e17336ae28
이명욱 <muk525@icloud.com>
2024-09-26 12:04:27 +0900

    design: search 아이콘 추가

--- front/src/pages/Stock/AllStock.tsx
+++ front/src/pages/Stock/AllStock.tsx
@@ -13,6 +13,7 @@ import { useState } from 'react';
 import { ButtonWrapper, SortButton } from '@features/Stock/styledComponent';
 import styled from 'styled-components';
 import useAllStockStore from '@store/useAllStockStore';
+import SearchIcon from '@features/Stock/AllStock/SearchIcon';
 
 const Underline = styled.div<{ $sortBy: string }>`
   position: absolute;
@@ -20,32 +21,48 @@ const Underline = styled.div<{ $sortBy: string }>`
   left: ${({ $sortBy }) => {
     switch ($sortBy) {
       case 'stckPrpr':
-        return '0.3rem'; 
+        return '0.3rem';
       case 'prdyCtrt':
-        return '4.3rem'; 
+        return '4.3rem';
       case 'acmlTrPbmn':
-        return '8.3rem'; 
+        return '8.3rem';
       case 'acmlVol':
-        return '12.3rem'; 
+        return '12.3rem';
       default:
         return '0.3rem';
     }
   }};
   height: 0.2rem;
   width: 2.4rem;
-  background-color: ${({ theme }) => theme.textColor}; 
-  transition: left 0.3s ease; 
+  background-color: ${({ theme }) => theme.textColor};
+  transition: left 0.3s ease;
 `;
 
-const InputTag = styled.input`
+const SearchInputWrapper = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const SearchInput = styled.input`
+  margin-top: 0.5rem;
   margin-bottom: 1rem;
-  padding: 0.5rem;
+  padding: 0.5rem 1rem 0.5rem 2.5rem;
   width: 100%;
   border-radius: 1rem;
   border: none;
   background-color: ${({ theme }) => theme.stockBackgroundColor};
   color: ${({ theme }) => theme.textColor};
   font-size: 1rem;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  left: 1rem;
+  top: 1.5rem;
+  transform: translateY(-50%);
+  display: flex;
+  align-items: center;
+  pointer-events: none;
 `;
 
 const AllStockPage: React.FC = () => {
@@ -95,18 +112,25 @@ const AllStockPage: React.FC = () => {
         <HrTag />
 
         {/* 검색창 추가 */}
-        <InputTag
-          type="text"
-          value={query}
-          onChange={handleInputChange}
-          placeholder="주식 검색"
-        />
+        <SearchInputWrapper>
+          <IconWrapper>
+            <SearchIcon />
+          </IconWrapper>
+          <SearchInput
+            type="text"
+            value={query}
+            onChange={handleInputChange}
+            placeholder="주식 종목 검색"
+          />
+        </SearchInputWrapper>
 
         {/* 정렬 버튼 추가 */}
         <ButtonWrapper>
           <SortButton onClick={() => setSortBy('stckPrpr')}>현재가</SortButton>
           <SortButton onClick={() => setSortBy('prdyCtrt')}>등락률</SortButton>
-          <SortButton onClick={() => setSortBy('acmlTrPbmn')}>거래금</SortButton>
+          <SortButton onClick={() => setSortBy('acmlTrPbmn')}>
+            거래금
+          </SortButton>
           <SortButton onClick={() => setSortBy('acmlVol')}>거래량</SortButton>
           <Underline $sortBy={sortBy} />
         </ButtonWrapper>
