bd1d17f307a7800d58dca4d97bcdb3704ebf4783
고민호 <koho10477@naver.com>
2024-09-26 11:01:02 +0900

    refactor : #81 @Repository 어노테이션 제거

--- back/stock/src/main/java/com/ssafy/stock/domain/repository/StocksRepository.java
+++ back/stock/src/main/java/com/ssafy/stock/domain/repository/StocksRepository.java
@@ -3,11 +3,9 @@ package com.ssafy.stock.domain.repository;
 import com.ssafy.stock.domain.entity.Stocks;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface StocksRepository extends JpaRepository<Stocks, Long> {
     Optional<Stocks> findByStockCode(String stockCode);
 
