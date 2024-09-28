763137040d1af178f1f5385b5fbdac52057982cc
thswltjr11 <thswltjr11@gmail.com>
2024-09-28 16:01:34 +0900

    refactor: #94 뉴스 컨텐츠 validator 통합

--- back/news/src/main/java/com/ssafy/news/domain/service/validator/NewsValidator.java
+++ back/news/src/main/java/com/ssafy/news/domain/service/validator/NewsValidator.java
@@ -1,7 +1,6 @@
 package com.ssafy.news.domain.service.validator;
 
 
-import com.ssafy.news.domain.entity.IndustryNews;
 import com.ssafy.news.global.exception.NewsNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,7 +9,7 @@ import java.util.List;
 @Slf4j
 public class NewsValidator {
     // 뉴스가 조회되지 않았을 때 처리하는 메서드
-    public static void validateNewsContent(List<IndustryNews> content) {
+    public static void validateNewsContent(List<?> content) {
         if (content.isEmpty()) {
             log.error("뉴스가 조회되지 않았습니다.");
             throw new NewsNotFoundException();
