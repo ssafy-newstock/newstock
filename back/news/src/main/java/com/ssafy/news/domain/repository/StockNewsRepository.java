ac6a0e27fb33f0289fbe319dd5cd945055ba070d
thswltjr11 <thswltjr11@gmail.com>
2024-09-28 16:05:25 +0900

    feat: #94 종목 뉴스 Top4 조회 쿼리 구현

--- back/news/src/main/java/com/ssafy/news/domain/repository/StockNewsRepository.java
+++ back/news/src/main/java/com/ssafy/news/domain/repository/StockNewsRepository.java
@@ -1,7 +1,13 @@
 package com.ssafy.news.domain.repository;
 
 import com.ssafy.news.domain.entity.StockNews;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface StockNewsRepository extends JpaRepository<StockNews, Integer> {
+    @Query("select s from StockNews s order by s.uploadDatetime desc")
+    List<StockNews> findTop4(Pageable pageable);
 }
