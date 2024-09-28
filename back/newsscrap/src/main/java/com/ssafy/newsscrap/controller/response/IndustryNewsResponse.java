e0263735d76b0a69cc72aebd95f042235eea7db1
손지석 <thswltjr11@gmail.com>
2024-09-28 11:27:03 +0900

    feat: #91 시황 뉴스 response 추가

---
+++ back/newsscrap/src/main/java/com/ssafy/newsscrap/controller/response/IndustryNewsResponse.java
@@ -0,0 +1,25 @@
+package com.ssafy.newsscrap.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class IndustryNewsResponse {
+    private String article;
+    private String description;
+    private String industry;
+    private String media;
+    private String newsId;
+    private String sentiment;
+    private String subtitle;
+    private String thumbnail;
+    private String title;
+    private LocalDateTime uploadDatetime;
+}
