558e4f84c4f124a818eab189a38711632b87f295
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-09-28 13:46:35 +0900

    refactor: 불필요한 태그 삭제(key)

--- front/src/features/Stock/SectionStock/Modal.tsx
+++ front/src/features/Stock/SectionStock/Modal.tsx
@@ -181,7 +181,6 @@ const Modal: React.FC<ModalProps> = ({ onClose, category }) => {
               <TextBold>거래량</TextBold>
             </StockCardRow>
             {filteredStocks?.map((stock) => (
-              <>
                 <StockCardRow
                   key={stock.stockCode}
                   onClick={() =>
@@ -208,7 +207,6 @@ const Modal: React.FC<ModalProps> = ({ onClose, category }) => {
                   <Text>{formatUnit(stock.acmlTrPbmn)}</Text>
                   <Text>{formatNumber(stock.acmlVol)}주</Text>
                 </StockCardRow>
-              </>
             ))}
           </div>
         )}
