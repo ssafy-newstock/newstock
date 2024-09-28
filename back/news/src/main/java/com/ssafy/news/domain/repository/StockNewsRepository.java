dd58626e5b18713391782ac534a297b20cb4d6a2
thswltjr11 <thswltjr11@gmail.com>
2024-09-28 16:20:44 +0900

    feat: #94 종목코드 뉴스 조회 쿼리 구현

--- back/news/src/main/java/com/ssafy/news/domain/repository/StockNewsRepository.java
+++ back/news/src/main/java/com/ssafy/news/domain/repository/StockNewsRepository.java
@@ -1,13 +1,22 @@
 package com.ssafy.news.domain.repository;
 
 import com.ssafy.news.domain.entity.StockNews;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface StockNewsRepository extends JpaRepository<StockNews, Integer> {
     @Query("select s from StockNews s order by s.uploadDatetime desc")
     List<StockNews> findTop4(Pageable pageable);
+
+    @Query("select s from StockNews s order by s.uploadDatetime desc")
+    Page<StockNews> findAllStockPage(Pageable pageable);
+
+    @Query("SELECT sn FROM StockNews sn JOIN sn.stockNewsStockCodes sc WHERE sc.stockCode = :stockCode")
+    Page<StockNews> findByStockCode(@Param("stockCode") String stockCode, Pageable pageable);
+
 }
