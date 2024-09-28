f433ee90463d6f19277faf7ee7d343631bc8bf8d
손지석 <thswltjr11@gmail.com>
2024-09-28 11:25:04 +0900

    feat: #91 관련 엔티티 생성

---
+++ back/newsscrap/src/main/java/com/ssafy/newsscrap/domain/entity/dto/FavoriteNewsDto.java
@@ -0,0 +1,45 @@
+package com.ssafy.newsscrap.domain.entity.dto;
+
+import com.ssafy.newsscrap.domain.IndustryNewsResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class FavoriteNewsDto {
+    private Long favoriteNewsId;
+    private String newsType;
+
+    private String article;
+    private String description;
+    private String industry;
+    private String media;
+    private String newsId;
+    private String sentiment;
+    private String subtitle;
+    private String thumbnail;
+    private String title;
+    private LocalDateTime uploadDatetime;
+
+    public static FavoriteNewsDto of(Long favoriteNewsId, IndustryNewsResponse response) {
+        FavoriteNewsDto favoriteNewsDto = new FavoriteNewsDto();
+        favoriteNewsDto.favoriteNewsId = favoriteNewsId;
+        favoriteNewsDto.industry = response.getIndustry();
+        favoriteNewsDto.article = response.getArticle();
+        favoriteNewsDto.description = response.getDescription();
+        favoriteNewsDto.media = response.getMedia();
+        favoriteNewsDto.newsId = response.getNewsId();
+        favoriteNewsDto.sentiment = response.getSentiment();
+        favoriteNewsDto.subtitle = response.getSubtitle();
+        favoriteNewsDto.thumbnail = response.getThumbnail();
+        favoriteNewsDto.title = response.getTitle();
+        favoriteNewsDto.uploadDatetime = response.getUploadDatetime();
+        return favoriteNewsDto;
+    }
+}
---
+++ back/newsscrap/src/test/java/com/ssafy/newsscrap/NewsscrapApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ssafy.newsscrap;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsscrapApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
