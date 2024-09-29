3bffe1b67575942b4cc2710e49f360559d80743c
thswltjr11 <thswltjr11@gmail.com>
2024-09-29 17:29:24 +0900

    feat: #99 뉴스 API 예외 핸들러 구현

--- back/news/src/main/java/com/ssafy/news/domain/controller/advice/NewsControllerAdvice.java
+++ back/news/src/main/java/com/ssafy/news/domain/controller/advice/NewsControllerAdvice.java
@@ -1,7 +1,9 @@
 package com.ssafy.news.domain.controller.advice;
 
 import com.ssafy.news.global.common.CommonResponse;
+import com.ssafy.news.global.exception.AlreadyFavoriteNews;
 import com.ssafy.news.global.exception.NewsNotFoundException;
+import com.ssafy.news.global.exception.NotExistFavoriteNews;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +12,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
-public class IndustryNewsControllerAdvice {
+public class NewsControllerAdvice {
     @ExceptionHandler(NewsNotFoundException.class)
     public ResponseEntity<?> handleNotFound(NewsNotFoundException e) {
         log.error(e.getMessage());
         return new ResponseEntity<>(CommonResponse.failure(e.getMessage()), HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(AlreadyFavoriteNews.class)
+    public ResponseEntity<?> handleAlreadyNews(AlreadyFavoriteNews e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(CommonResponse.failure(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotExistFavoriteNews.class)
+    public ResponseEntity<?> handleNotExistFavoriteNews(NotExistFavoriteNews e) {
+        log.error(e.getMessage());
+        return new ResponseEntity<>(CommonResponse.failure(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+
 }
