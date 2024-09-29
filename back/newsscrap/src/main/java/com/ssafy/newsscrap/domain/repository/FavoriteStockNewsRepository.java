f630ac28f874754261e17c085126463f3f3b8f9a
thswltjr11 <thswltjr11@gmail.com>
2024-09-29 16:29:34 +0900

    fix: #99 관심 기능 news 마이크로서비스로 이동

--- back/newsscrap/src/main/java/com/ssafy/newsscrap/domain/repository/FavoriteStockNewsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.newsscrap.domain.repository;
-
-import com.ssafy.newsscrap.domain.entity.FavoriteStockNews;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteStockNewsRepository extends JpaRepository<FavoriteStockNews, Long> {
-}
