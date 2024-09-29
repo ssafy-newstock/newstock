3fba44de7089eed5d16019c7ba7cc3085d3c2b3d
thswltjr11 <thswltjr11@gmail.com>
2024-09-29 16:47:36 +0900

    feat: #99 Auth 서비스의 Token 검증 로직 구현

---
+++ back/news/src/main/java/com/ssafy/news/global/exception/MemberIdNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ssafy.news.global.exception;
+
+public class MemberIdNotFoundException extends RuntimeException {
+    public MemberIdNotFoundException() {
+        super("잘못된 형식의 토큰으로 memberId를 찾지 못했습니다.");
+    }
+
+    public MemberIdNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ back/news/src/main/java/com/ssafy/news/global/util/TokenProvider.java
@@ -0,0 +1,25 @@
+package com.ssafy.news.global.util;
+
+import com.ssafy.news.domain.service.client.AuthClient;
+import com.ssafy.news.domain.service.client.request.MemberIdRequest;
+import com.ssafy.news.domain.service.client.response.MemberIdResponse;
+import com.ssafy.news.global.exception.MemberIdNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class TokenProvider {
+    private final AuthClient authClient;
+
+    public Long getMemberId(String token) {
+        MemberIdResponse body = authClient.getMemberId(new MemberIdRequest(token));
+        log.info("body={}", body);
+        if (body == null || body.getMemberId() == null) {
+            throw new MemberIdNotFoundException();
+        }
+        return body.getMemberId();
+    }
+}
