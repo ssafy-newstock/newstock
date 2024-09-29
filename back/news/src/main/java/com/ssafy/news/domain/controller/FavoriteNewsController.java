947f88292a68c872293b147a349f9727223f9a21
thswltjr11 <thswltjr11@gmail.com>
2024-09-29 16:51:55 +0900

    feat: #99 뉴스 관심 목록 조회 API 구현

--- back/news/src/main/java/com/ssafy/news/domain/controller/FavoriteNewsController.java
+++ back/news/src/main/java/com/ssafy/news/domain/controller/FavoriteNewsController.java
@@ -1,11 +1,15 @@
 package com.ssafy.news.domain.controller;
 
+import com.ssafy.news.domain.entity.dto.IndustryNewsDto;
+import com.ssafy.news.domain.entity.dto.StockNewsDto;
 import com.ssafy.news.domain.service.FavoriteIndustryNewsService;
 import com.ssafy.news.domain.service.FavoriteStockNewsService;
 import com.ssafy.news.global.common.CommonResponse;
 import com.ssafy.news.global.util.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/news/favorite")
@@ -33,6 +37,17 @@ public class FavoriteNewsController {
         return CommonResponse.success("성공");
     }
 
+    @GetMapping("/industry/list")
+    public CommonResponse<?> getFavoriteIndustryNews(
+            @RequestHeader("authorization") String token,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+        Long memberId = tokenProvider.getMemberId(token);
+
+        List<IndustryNewsDto> favoriteIndustryNews = favoriteIndustryNewsService.getFavoriteIndustryNews(memberId, page, size);
+        return CommonResponse.success(favoriteIndustryNews);
+    }
+
     @PostMapping("/stock/{id}")
     public CommonResponse<?> favoriteStockNews(
             @RequestHeader("authorization") String token,
@@ -49,5 +64,16 @@ public class FavoriteNewsController {
         Long memberId = tokenProvider.getMemberId(token);
         favoriteStockNewsService.unFavoriteNews(memberId, newsId);
         return CommonResponse.success("성공");
+    }
+
+    @GetMapping("/stock/list")
+    public CommonResponse<?> getFavoriteStockNews(
+            @RequestHeader("authorization") String token,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+        Long memberId = tokenProvider.getMemberId(token);
+
+        List<StockNewsDto> favoriteStockNews = favoriteStockNewsService.getFavoriteStockNews(memberId, page, size);
+        return CommonResponse.success(favoriteStockNews);
     }
 }
