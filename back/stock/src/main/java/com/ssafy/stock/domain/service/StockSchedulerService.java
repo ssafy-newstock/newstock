5edb1e3709b98e28259b0212da988f3bf6f965aa
고민호 <koho10477@naver.com>
2024-09-30 09:11:23 +0900

    feat : #95 장 종료시 주식 가격 Redis -> DBMS 저장 스케줄러 적용

--- back/stock/src/main/java/com/ssafy/stock/domain/service/StockSchedulerService.java
+++ back/stock/src/main/java/com/ssafy/stock/domain/service/StockSchedulerService.java
@@ -2,8 +2,16 @@ package com.ssafy.stock.domain.service;
 
 import com.ssafy.stock.domain.entity.Redis.StocksPriceLiveDailyChartRedis;
 import com.ssafy.stock.domain.entity.Redis.StocksPriceLiveRedis;
-import com.ssafy.stock.domain.repository.StocksPriceLiveRedisRepository;
+import com.ssafy.stock.domain.entity.Redis.StocksPriceRedis;
+import com.ssafy.stock.domain.entity.STOCKPRICE;
+import com.ssafy.stock.domain.entity.Stocks;
+import com.ssafy.stock.domain.entity.StocksPrice;
+import com.ssafy.stock.domain.error.custom.StockNotFoundException;
+import com.ssafy.stock.domain.repository.redis.StocksPriceLiveRedisRepository;
+import com.ssafy.stock.domain.repository.StocksPriceRepository;
+import com.ssafy.stock.domain.repository.StocksRepository;
 import com.ssafy.stock.domain.repository.redis.StocksPriceLiveDailyChartRedisRepository;
+import com.ssafy.stock.domain.repository.redis.StocksPriceRedisRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -19,7 +27,10 @@ import java.time.LocalDateTime;
 public class StockSchedulerService {
 
     private final StocksPriceLiveRedisRepository stocksPriceLiveRedisRepository;
+    private final StocksPriceRedisRepository stocksPriceRedisRepository;
     private final StocksPriceLiveDailyChartRedisRepository stocksPriceLiveDailyChartRedisRepository;
+    private final StocksPriceRepository stocksPriceRepository;
+    private final StocksRepository stocksRepository;
 
     /**
      * TOP10 주식 당일 차트 정보 저장 스케줄러
@@ -40,10 +51,68 @@ public class StockSchedulerService {
     }
 
     /**
-     * 매일 자정 당일 차트 데이터 삭제
+     * 장 시작전 전일 데일리 차트 데이터 삭제
      */
-    @Scheduled(cron = "0 0 0 * * MON-FRI", zone = "Asia/Seoul")
+    @Scheduled(cron = "0 50 8 * * MON-FRI", zone = "Asia/Seoul")
     public void deleteStockPriceLiveDailyChart(){
         stocksPriceLiveDailyChartRedisRepository.deleteAll();
     }
+
+    /**
+     * 장 종료시 현재가를 Redis -> DBMS 에 저장하기
+     */
+    @Transactional
+    @Scheduled(cron = "0 30 15 * * MON-SUN", zone = "Asia/Seoul")
+    public void saveStocksPrice() {
+        Iterable<StocksPriceRedis> stocksPriceRedis = stocksPriceRedisRepository.findAll();
+
+        for (StocksPriceRedis stockPriceRedis : stocksPriceRedis) {
+            Stocks stock = stocksRepository.findByStockCodeWithStockPrice(stockPriceRedis.getStockCode())
+                    .orElseThrow(() -> new StockNotFoundException());
+
+            StocksPrice findStockPrice = stock.getStockPrice();
+
+            if(findStockPrice == null){
+                stocksPriceRepository.save(new StocksPrice(stock,
+                        STOCKPRICE.OTHER,
+                        stockPriceRedis.getStckPrpr(),
+                        stockPriceRedis.getPrdyVrss(),
+                        stockPriceRedis.getPrdyCtrt(),
+                        stockPriceRedis.getAcmlTrPbmn(),
+                        stockPriceRedis.getAcmlVol()));
+            } else {
+                findStockPrice.update(stockPriceRedis.getStckPrpr(),
+                        stockPriceRedis.getPrdyVrss(),
+                        stockPriceRedis.getPrdyCtrt(),
+                        stockPriceRedis.getAcmlTrPbmn(),
+                        stockPriceRedis.getAcmlVol());
+            }
+        }
+
+        Iterable<StocksPriceLiveRedis> stocksPriceLiveRedis = stocksPriceLiveRedisRepository.findAll();
+
+        for (StocksPriceLiveRedis stockPriceLiveRedis : stocksPriceLiveRedis) {
+            Stocks stock = stocksRepository.findByStockCodeWithStockPrice(stockPriceLiveRedis.getStockCode())
+                    .orElseThrow(() -> new StockNotFoundException());
+
+            StocksPrice findStockPrice = stock.getStockPrice();
+
+            if(findStockPrice == null){
+                stocksPriceRepository.save(new StocksPrice(stock,
+                        STOCKPRICE.TOP,
+                        stockPriceLiveRedis.getStckPrpr(),
+                        stockPriceLiveRedis.getPrdyVrss(),
+                        stockPriceLiveRedis.getPrdyCtrt(),
+                        stockPriceLiveRedis.getAcmlTrPbmn(),
+                        stockPriceLiveRedis.getAcmlVol()));
+            } else {
+                findStockPrice.update(stockPriceLiveRedis.getStckPrpr(),
+                        stockPriceLiveRedis.getPrdyVrss(),
+                        stockPriceLiveRedis.getPrdyCtrt(),
+                        stockPriceLiveRedis.getAcmlTrPbmn(),
+                        stockPriceLiveRedis.getAcmlVol());
+            }
+        }
+    }
+
 }
