2f63380c8dee2b4521063a03c40ced599af87705
고민호 <koho10477@naver.com>
2024-09-30 09:10:56 +0900

    feat : #95 주식 주식가격 패치 조인 추가

--- back/stock/src/main/java/com/ssafy/stock/domain/repository/StocksRepository.java
+++ back/stock/src/main/java/com/ssafy/stock/domain/repository/StocksRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.stock.domain.repository;
 
+import com.ssafy.stock.domain.entity.STOCKPRICE;
 import com.ssafy.stock.domain.entity.Stocks;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,21 +14,21 @@ public interface StocksRepository extends JpaRepository<Stocks, Long> {
 
     List<Stocks> findByStockCodeIn(List<String> stockCodes);
     @Query("SELECT s " +
-            "FROM Stocks s " +
-            "LEFT JOIN FETCH s.stocksCandles " +
-            "WHERE s.stockCode = :stockCode")
-    Optional<Stocks> findByStockCodeWithCandles(String stockCode);
-
-    @Query("SELECT s " +
             "FROM Stocks s LEFT JOIN FETCH s.stocksCandles c " +
             "WHERE s.stockCode = :stockCode " +
             "AND c.stockCandleDay BETWEEN :startDate AND :endDate")
     Optional<Stocks> findByStockCodeWithCandlesAndDate(String stockCode, LocalDate startDate, LocalDate endDate);
 
+    @Query("SELECT s " +
+            "FROM Stocks s " +
+            "LEFT JOIN FETCH s.stockPrice " +
+            "WHERE s.stockCode = :stockCode")
+    Optional<Stocks> findByStockCodeWithStockPrice(String stockCode);
 
     @Query("SELECT s " +
-            "FROM Stocks s LEFT JOIN FETCH s.stocksCandles c " +
-            "WHERE s.stockIndustry = :stockIndustry ")
-    List<Stocks> findAllByStockIndustryWithCandle(String stockIndustry);
+            "FROM Stocks s " +
+            "LEFT JOIN FETCH s.stockPrice " +
+            "WHERE s.stockPrice.stockPriceType = :stockprice")
+    List<Stocks> findAllWithStockPrice(STOCKPRICE stockprice);
 
 }
