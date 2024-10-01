266f4a61f2ff3ca8fd9a41fd6ad6b8cff075517c
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-10-01 14:02:15 +0900

    feat: stock 관련 리액트 쿼리 분리

---
+++ front/src/hooks/useAllStockQuery.ts
@@ -0,0 +1,24 @@
+import { axiosInstance } from '@api/axiosInstance';
+import { IApiStock } from '@features/Stock/types';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+
+const fetchAllStock = () =>
+  axiosInstance.get<IApiStock>('/stock/price-list').then((res) => res.data);
+
+export const useAllStockQuery = (
+  options: {
+    enabled?: boolean;
+    refetchInterval?: number;
+    onError?: (error: Error) => void;
+    onSuccess?: (data: IApiStock) => void;
+  } = {}
+): UseQueryResult<IApiStock, Error> => {
+  return useQuery<IApiStock, Error>({
+    queryKey: ['allStockData'],
+    queryFn: fetchAllStock,
+    // staleTime: 1000 * 60 * 1, // 기본값 0(신선한 데이터)
+    gcTime: 1000 * 60 * 5, // 기본값 5분
+    retry: 3, // 재시도 3번
+    ...options, // 추가 옵션 사용
+  });
+};
---
+++ front/src/hooks/useFavortiteStockQuery.ts
@@ -0,0 +1,27 @@
+import { authRequest } from '@api/axiosInstance';
+import { IApiFavorite } from '@features/Stock/types';
+import {
+  UseSuspenseQueryResult,
+  useSuspenseQuery,
+} from '@tanstack/react-query';
+
+const fetchFavoriteStock = () =>
+  authRequest.get<IApiFavorite>('/stock/favorite').then((res) => res.data);
+
+export const useFavoriteStockQuery = (
+  options: {
+    enabled?: boolean;
+    refetchInterval?: number;
+    onError?: (error: Error) => void;
+    onSuccess?: (data: IApiFavorite) => void;
+  } = {}
+): UseSuspenseQueryResult<IApiFavorite, Error> => {
+  return useSuspenseQuery<IApiFavorite, Error>({
+    queryKey: ['favoriteStockData'],
+    queryFn: fetchFavoriteStock,
+    staleTime: 1000 * 60 * 1, // 기본값 0(신선한 데이터)
+    gcTime: 1000 * 60 * 5, // 기본값 5분
+    retry: 3, // 재시도 3번
+    ...options, // 추가 옵션 사용
+  });
+};
