a46f0e29a5514ee5441f171da10bad2ced204408
sunhong <psh56750855@gmail.com>
2024-10-02 01:41:43 +0900

    chore: #111 주석 제거

--- data/news_ai_server/app/api/main.py
+++ data/news_ai_server/app/api/main.py
@@ -4,7 +4,5 @@ from fastapi import APIRouter
 from api.routes import similarity
 
 api_router = APIRouter()
-# api_router.include_router(login.router, tags=["login"])
+
 api_router.include_router(similarity.router, prefix="/similarity", tags=["similarity"])
-# api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-# api_router.include_router(items.router, prefix="/items", tags=["items"])
