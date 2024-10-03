fdf993b063dc6f270a90d35e9ccf39da6b9763f0
thswltjr11 <thswltjr11@gmail.com>
2024-10-04 00:48:20 +0900

    fix: #126 CORS Config 추가

---
+++ back/favorite/src/main/java/com/ssafy/favorite/global/config/OriginConfig.java
@@ -0,0 +1,33 @@
+package com.ssafy.favorite.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Configuration
+public class OriginConfig implements WebMvcConfigurer {
+
+    private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,DELETE,TRACE,OPTIONS,PATCH,PUT";
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://newstock.info",
+                        "https://newstock.info:8000",
+                        "https://newstock.info:8001",
+                        "https://newstock.info:8002",
+                        "https://newstock.info:8003",
+                        "http://localhost:8000",
+                        "http://localhost:8001",
+                        "http://localhost:8002",
+                        "http://localhost:8003",
+                        "http://localhost:5173",
+                        "http://localhost:3000")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders(AUTHORIZATION);
+    }
+}
