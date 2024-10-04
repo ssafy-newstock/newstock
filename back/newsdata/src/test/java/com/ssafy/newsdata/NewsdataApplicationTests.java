a03b71b710dff74c168eebd42ca92f0829deb7b0
thswltjr11 <thswltjr11@gmail.com>
2024-10-05 00:35:04 +0900

    fix: #132 버그 수정

--- back/newsdata/src/test/java/com/ssafy/newsdata/NewsdataApplicationTests.java
+++ back/newsdata/src/test/java/com/ssafy/newsdata/NewsdataApplicationTests.java
@@ -1,13 +1,12 @@
 package com.ssafy.newsdata;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class NewsdataApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
