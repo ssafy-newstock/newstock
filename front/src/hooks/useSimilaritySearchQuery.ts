4673527006fb9500c221b02d31b43fed2f103300
LEE MYEONGUK <123948643+LEEMYEONGUK@users.noreply.github.com>
2024-10-05 23:17:01 +0900

    refactor: 차트 유사도 검색 요청 URL 변경

--- front/src/hooks/useSimilaritySearchQuery.ts
+++ front/src/hooks/useSimilaritySearchQuery.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { SimilaritySearchParams, SimilaritySearchResponse } from '@features/Stock/types';
+import { axiosInstance } from '@api/axiosInstance';
 
 export function useSimilaritySearchQuery(params: SimilaritySearchParams, isEnabled: boolean = false) {
   const { stockCode, start_date, end_date } = params;
@@ -8,7 +9,7 @@ export function useSimilaritySearchQuery(params: SimilaritySearchParams, isEnabl
   return useQuery<SimilaritySearchResponse>({
     queryKey: ['similaritySearch', stockCode, start_date, end_date],
     queryFn: async () => {
-      const { data } = await axios.get<SimilaritySearchResponse>('http://localhost:8001/similarity', {
+      const { data } = await axiosInstance.get<SimilaritySearchResponse>('/newsai/similarity/', {
         params: { base_stock_code: stockCode, start_date, end_date },
       });
       return data;
