d424492048f9dab9b06472821c9d8a7da9e609d0
고민호 <koho10477@naver.com>
2024-10-06 00:04:02 +0900

    feat : #135 멤버 수익률 응답 dto

---
+++ back/member/src/main/java/com/ssafy/member/domain/entity/dto/MemberChangeRateDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.member.domain.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class MemberChangeRateDto {
+    private Long memberId;
+    private Double changeRate;
+}
