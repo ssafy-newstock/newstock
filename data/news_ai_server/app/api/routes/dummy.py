c37ef33de45f60900ca70d3531ca925e590f51b2
sunhong <psh56750855@gmail.com>
2024-10-06 22:17:05 +0900

    fix: 307 redirect 원인 수정

--- data/news_ai_server/app/api/routes/dummy.py
+++ data/news_ai_server/app/api/routes/dummy.py
@@ -8,7 +8,7 @@ from utils import connect_jdbc
 router = APIRouter()
 date_pattern = r"^\d{4}-\d{2}-\d{2}$"
 
-@router.get("/")
+@router.get("")
 def get_dummy():
     print("Dummy Controller")
     print("Dummy Connect Success")
