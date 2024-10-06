8cdc6548f96f336fba5f9a4f7ccefb5af89f6016
이주호 <zxxv1245@naver.com>
2024-10-07 07:27:30 +0900

    bugfix: #145 styled-component 경고 문구 제거

--- front/src/features/MyStockModal/Icon.tsx
+++ front/src/features/MyStockModal/Icon.tsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
-const StyledSVGfill = styled.svg<{ active: boolean }>`
-  fill: ${({ active }) =>
-    active ? '#1a73e8' : '#828282'}; // active 상태에 따라 색상 변경
+const StyledSVGfill = styled.svg<{ $active: boolean }>`
+  fill: ${({ $active }) =>
+    $active ? '#1a73e8' : '#828282'}; // active 상태에 따라 색상 변경
 `;
 
-const StyledSVGstroke = styled.svg<{ active: boolean }>`
-  stroke: ${({ active }) =>
-    active ? '#1a73e8' : '#828282'}; // active 상태에 따라 색상 변경
+const StyledSVGstroke = styled.svg<{ $active: boolean }>`
+  stroke: ${({ $active }) =>
+    $active ? '#1a73e8' : '#828282'}; // active 상태에 따라 색상 변경
 `;
 
 interface IconProps {
-  active: boolean;
+  $active: boolean;
 }
 
 const OverflowIcon: React.FC = () => (
@@ -27,14 +27,14 @@ const OverflowIcon: React.FC = () => (
   </svg>
 );
 
-const MyStockIcon: React.FC<IconProps> = ({ active }) => (
+const MyStockIcon: React.FC<IconProps> = ({ $active }) => (
   <StyledSVGstroke
     xmlns="http://www.w3.org/2000/svg"
     width="2rem"
     height="2rem"
     viewBox="0 0 24 24"
     fill="none"
-    active={active}
+    $active={$active}
   >
     <path
       d="M4.58 8.60696L2 8.45396C3.849 3.70396 9.158 0.999958 14.333 2.34396C19.846 3.77696 23.121 9.26196 21.647 14.594C20.428 19.005 16.343 21.931 11.847 22"
@@ -51,13 +51,13 @@ const MyStockIcon: React.FC<IconProps> = ({ active }) => (
   </StyledSVGstroke>
 );
 
-const HistoryIcon: React.FC<IconProps> = ({ active }) => (
+const HistoryIcon: React.FC<IconProps> = ({ $active }) => (
   <StyledSVGstroke
     xmlns="http://www.w3.org/2000/svg"
     width="2rem"
     height="2rem"
     viewBox="0 0 24 24"
-    active={active}
+    $active={$active}
   >
     <g fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
       <path d="M5.636 18.364A9 9 0 1 0 3 12.004V14" />
@@ -66,37 +66,37 @@ const HistoryIcon: React.FC<IconProps> = ({ active }) => (
   </StyledSVGstroke>
 );
 
-const HeartIcon: React.FC<IconProps> = ({ active }) => (
+const HeartIcon: React.FC<IconProps> = ({ $active }) => (
   <StyledSVGfill
     xmlns="http://www.w3.org/2000/svg"
     width="2rem"
     height="2rem"
     viewBox="0 0 24 24"
-    active={active}
+    $active={$active}
   >
     <path d="m12.1 18.55l-.1.1l-.11-.1C7.14 14.24 4 11.39 4 8.5C4 6.5 5.5 5 7.5 5c1.54 0 3.04 1 3.57 2.36h1.86C13.46 6 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5c0 2.89-3.14 5.74-7.9 10.05M16.5 3c-1.74 0-3.41.81-4.5 2.08C10.91 3.81 9.24 3 7.5 3C4.42 3 2 5.41 2 8.5c0 3.77 3.4 6.86 8.55 11.53L12 21.35l1.45-1.32C18.6 15.36 22 12.27 22 8.5C22 5.41 19.58 3 16.5 3" />
   </StyledSVGfill>
 );
 
-const StarIcon: React.FC<IconProps> = ({ active }) => (
+const StarIcon: React.FC<IconProps> = ({ $active }) => (
   <StyledSVGfill
     xmlns="http://www.w3.org/2000/svg"
     width="2rem"
     height="2rem"
     viewBox="0 0 24 24"
-    active={active}
+    $active={$active}
   >
     <path d="m12 15.39l-3.76 2.27l.99-4.28l-3.32-2.88l4.38-.37L12 6.09l1.71 4.04l4.38.37l-3.32 2.88l.99 4.28M22 9.24l-7.19-.61L12 2L9.19 8.63L2 9.24l5.45 4.73L5.82 21L12 17.27L18.18 21l-1.64-7.03z" />
   </StyledSVGfill>
 );
 
-const RankIcon: React.FC<IconProps> = ({ active }) => (
+const RankIcon: React.FC<IconProps> = ({ $active }) => (
   <StyledSVGstroke
     xmlns="http://www.w3.org/2000/svg"
     width="2rem"
     height="2rem"
     viewBox="0 0 48 48"
-    active={active}
+    $active={$active}
   >
     <g fill="none" strokeLinejoin="round" strokeWidth="4">
       <path strokeLinecap="round" d="M17 18H4v24h13z" />
--- front/src/features/MyStockModal/StockModal.tsx
+++ front/src/features/MyStockModal/StockModal.tsx
@@ -30,11 +30,11 @@ interface StockModalProps {
 }
 
 // active 상태에 따른 TextP_12 스타일링
-const ActiveTextP_12 = styled(TextP_12)<{ active: boolean }>`
-  color: ${({ active }) =>
-    active ? '#1a73e8' : '#828282'}; // 활성화된 텍스트도 동일한 색상 변경
-  font-weight: ${({ active }) =>
-    active ? '600' : 'normal'}; // 선택된 메뉴는 볼드 처리
+const ActiveTextP_12 = styled(TextP_12)<{ $active: boolean }>`
+  color: ${({ $active }) =>
+    $active ? '#1a73e8' : '#828282'}; // 활성화된 텍스트도 동일한 색상 변경
+  font-weight: ${({ $active }) =>
+    $active ? '600' : 'normal'}; // 선택된 메뉴는 볼드 처리
 `;
 
 const StockModal: React.FC<StockModalProps> = ({ isOpen, setIsOpen }) => {
@@ -84,32 +84,32 @@ const StockModal: React.FC<StockModalProps> = ({ isOpen, setIsOpen }) => {
         </ModalLeft>
         <ModalRight>
           <IconDiv onClick={() => setActiveComponent('MyStock')}>
-            <MyStockIcon active={activeComponent === 'MyStock'} />
-            <ActiveTextP_12 active={activeComponent === 'MyStock'}>
+            <MyStockIcon $active={activeComponent === 'MyStock'} />
+            <ActiveTextP_12 $active={activeComponent === 'MyStock'}>
               내 주식
             </ActiveTextP_12>
           </IconDiv>
           <IconDiv onClick={() => setActiveComponent('MyStockHistory')}>
-            <HistoryIcon active={activeComponent === 'MyStockHistory'} />
-            <ActiveTextP_12 active={activeComponent === 'MyStockHistory'}>
+            <HistoryIcon $active={activeComponent === 'MyStockHistory'} />
+            <ActiveTextP_12 $active={activeComponent === 'MyStockHistory'}>
               거래 내역
             </ActiveTextP_12>
           </IconDiv>
           <IconDiv onClick={() => setActiveComponent('Heart')}>
-            <HeartIcon active={activeComponent === 'Heart'} />
-            <ActiveTextP_12 active={activeComponent === 'Heart'}>
+            <HeartIcon $active={activeComponent === 'Heart'} />
+            <ActiveTextP_12 $active={activeComponent === 'Heart'}>
               관심 종목
             </ActiveTextP_12>
           </IconDiv>
           <IconDiv onClick={() => setActiveComponent('Star')}>
-            <StarIcon active={activeComponent === 'Star'} />
-            <ActiveTextP_12 active={activeComponent === 'Star'}>
+            <StarIcon $active={activeComponent === 'Star'} />
+            <ActiveTextP_12 $active={activeComponent === 'Star'}>
               관심 뉴스
             </ActiveTextP_12>
           </IconDiv>
           <IconDiv onClick={() => setActiveComponent('Rank')}>
-            <RankIcon active={activeComponent === 'Rank'} />
-            <ActiveTextP_12 active={activeComponent === 'Rank'}>
+            <RankIcon $active={activeComponent === 'Rank'} />
+            <ActiveTextP_12 $active={activeComponent === 'Rank'}>
               랭킹
             </ActiveTextP_12>
           </IconDiv>
