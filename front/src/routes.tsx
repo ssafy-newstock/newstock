0bfbb73398a5dabd010010ed64e7585406e5bd1c
이명욱 <muk525@icloud.com>
2024-10-07 15:48:06 +0900

    chore: 불필요한 임포트 삭제

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -29,9 +29,6 @@ import MyStockPage from '@pages/Stock/MyStock';
 import AllStockPage from '@pages/Stock/AllStock';
 import SectionStockPage from '@pages/Stock/SectionStock';
 import Auth from '@pages/Auth';
-import NewsMainSkeleton from '@features/News/skeleton/NewsMainSkeleton';
-import EconNewsSkeleton from '@features/News/skeleton/EconNewsSkeleton';
-import StockNewsSkeleton from '@features/News/skeleton/StockNewsSkeleton';
 
 const router = createBrowserRouter([
   {
