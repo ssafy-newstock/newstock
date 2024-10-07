7a6fd9e5d5f24f5fa37452aff6274c58d200af67
sunhong <psh56750855@gmail.com>
2024-10-07 10:54:55 +0900

    feat: redirect 로직 수정

--- data/news_ai_server/app/api/main.py
+++ data/news_ai_server/app/api/main.py
@@ -10,6 +10,9 @@ app = FastAPI(
 
 api_router = APIRouter()
 
+# 307 redirect
+app.router.redirect_slashes = False
+
 api_router.include_router(similarity.router, prefix="/api/newsai/similarity", tags=["similarity"])
 api_router.include_router(summary.router, prefix="/api/newsai/summary", tags=["summary"])
 api_router.include_router(dummy.router, prefix="/api/newsai/dummy", tags=["dummy"])
