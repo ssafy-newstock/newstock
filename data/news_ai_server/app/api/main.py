aaa4784dc704d2643c4da843b904d538abdeeb4b
sunhong <psh56750855@gmail.com>
2024-10-09 01:38:03 +0900

    feat: api 추가

--- data/news_ai_server/app/api/main.py
+++ data/news_ai_server/app/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 # from api.routes import items, login, utils
-from api.routes import similarity, summary, dummy, short
+from api.routes import similarity, summary, dummy, short, chat
 from fastapi import FastAPI, WebSocket
 from starlette.websockets import WebSocketDisconnect
 app = FastAPI(
@@ -16,5 +16,6 @@ api_router = APIRouter()
 api_router.include_router(similarity.router, prefix="/api/newsai/similarity", tags=["similarity"])
 api_router.include_router(summary.router, prefix="/api/newsai/summary", tags=["summary"])
 api_router.include_router(short.router, prefix="/api/newsai/short", tags=["short"])
+api_router.include_router(chat.router, prefix="/api/newsai/chat", tags=["chat"])
 api_router.include_router(dummy.router, prefix="/api/newsai/dummy", tags=["dummy"])
 
