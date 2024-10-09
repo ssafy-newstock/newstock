19e90dddbca6a78fd1bd5935dc0625060f8c7d0d
이주호 <zxxv1245@naver.com>
2024-10-10 03:25:03 +0900

    chore: #166 css 분리

---
+++ front/src/features/Etc/AIChatBot/AIChatBotStyledComponent.ts
@@ -0,0 +1,197 @@
+import styled from 'styled-components';
+
+export const ChatCenter = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+`;
+
+export const DateWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 1rem;
+  cursor: pointer;
+`;
+
+export const ChatOuterWrapper = styled.div`
+  width: 100%;
+  max-width: 106rem;
+  height: 95%;
+  display: flex;
+  padding: 0.94rem 1.25rem;
+  margin: 1.25rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  border-radius: 1.875rem;
+`;
+
+export const ChatBodyWrapper = styled.div`
+  width: 100%;
+  height: 90%;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  &::-webkit-scrollbar {
+    width: 0;
+    height: 0;
+  }
+`;
+
+export const ChatMessageOuterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ChatMessageInnerWrapper = styled.div<{ $isMine: boolean }>`
+  display: flex;
+  gap: 1.25rem;
+  justify-content: ${({ $isMine }) => ($isMine ? 'flex-end' : 'flex-start')};
+  align-items: center;
+  width: 100%;
+`;
+
+export const ChatImage = styled.img`
+  width: auto;
+  height: 100%;
+  max-height: 2.5rem;
+`;
+
+export const ChatMessage = styled.div<{ $isMine: boolean }>`
+  margin: 0.31rem 0;
+  padding: 0.625rem;
+  background-color: ${({ $isMine, theme }) =>
+    $isMine ? theme.chatBalloonColor : 'transparent'};
+  color: ${({ theme }) => theme.textColor};
+  max-width: ${({ $isMine }) => ($isMine ? '60%' : '100%')};
+  word-wrap: break-word;
+  border-radius: ${({ $isMine }) => ($isMine ? '0.5rem' : '0')};
+  text-align: ${({ $isMine }) => ($isMine ? 'right' : 'left')};
+`;
+
+export const ChatInputWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  border-radius: 1.875rem;
+  border: 1px solid #ccc;
+  position: relative;
+
+  /* 포커스 시 스타일 */
+  &:focus-within {
+    border-color: ${({ theme }) =>
+      theme.highlightColor}; /* 포커스 시 하이라이트 색상 */
+    box-shadow: 0 0 5px ${({ theme }) => theme.highlightColor}; /* 포커스 시 외곽선 강조 */
+  }
+`;
+
+export const ChatInput = styled.input`
+  flex: 1;
+  padding: 0.94rem 1.25rem 0.94rem 0.1rem;
+  border-radius: 1.875rem;
+  border: none;
+  background-color: transparent;
+  color: ${({ theme }) => theme.textColor};
+  font-size: 1rem;
+  outline: none;
+`;
+
+export const DatePickerWrapper = styled.div`
+  display: flex;
+  margin-right: auto;
+  margin-left: 1rem;
+  gap: 1rem;
+  align-items: center;
+`;
+
+export const SendIconWrapper = styled.div`
+  margin-left: 0.625rem;
+  margin-right: 0.625rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+`;
+
+export const SendIcon = styled.svg`
+  fill: ${({ theme }) => theme.sendIconColor};
+`;
+
+export const DatePickerText = styled.p`
+  color: ${({ theme }) => theme.profileColor};
+  font-weight: 500;
+  font-size: 1.2rem;
+`;
+
+export const CustomInput = styled.input`
+  padding: 0.5rem;
+  border-radius: 1rem;
+  border: none;
+  background-color: ${({ theme }) => theme.profileBackgroundColor};
+  color: ${({ theme }) => theme.profileColor};
+  font-size: 1rem;
+  font-weight: 500;
+  width: 100%;
+  outline: none;
+  cursor: pointer;
+  text-align: center;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.profileColor};
+    opacity: 0.7;
+  }
+`;
+
+export const NewsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  width: 100%;
+  margin-top: 1.25rem;
+  gap: 1.25rem;
+`;
+
+export const NewsHeaderText = styled.p`
+  font-weight: 600;
+  font-size: 1.25rem;
+`;
+
+export const NewsOuterWrapper = styled.div`
+  display: flex;
+  overflow-x: auto;
+  width: 100%;
+  gap: 1rem;
+`;
+
+export const NewsItemWrapper = styled.div`
+  flex: 0 0 auto;
+  width: 18%;
+  height: 12.5rem;
+  padding: 1.25rem 1rem;
+  border: 1px solid #d1d1d1;
+  border-radius: 1rem;
+  background-color: ${({ theme }) => theme.newsBackgroundColor};
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  cursor: pointer;
+`;
+
+export const NewsMedia = styled.h3`
+  font-size: 1.1rem;
+  font-weight: 600;
+`;
+
+export const NewsTitle = styled.p`
+  font-size: 1.2rem;
+`;
+
+export const NewsDate = styled.p`
+  font-size: 0.8rem;
+  margin-bottom: 1rem;
+`;
--- front/src/pages/Etc/AIChatBot.tsx
+++ front/src/pages/Etc/AIChatBot.tsx
@@ -11,7 +11,7 @@ import styled, { useTheme } from 'styled-components';
 import chatbotImg from '@assets/Chat/chatbotImg.png';
 import darkUserImg from '@assets/Chat/darkUser.png';
 import lightUserImg from '@assets/Chat/user.png';
-import AINews from '@features/Etc/AINews';
+import AINews from '@features/Etc/AIChatBot/AINews';
 import WelcomeMessage from '@features/Etc/WelcomeMessage';
 import CalendarIcon from '@features/MyNews/CalendarIcon';
 import DatePicker from 'react-datepicker';
@@ -19,134 +19,23 @@ import 'react-datepicker/dist/react-datepicker.css';
 import { axiosInstance } from '@api/axiosInstance';
 import { useChatStore, Message } from '@store/useChatBotStore';
 import { ko } from 'date-fns/locale';
-
-const ChatCenter = styled.div`
-  display: flex;
-  width: 100%;
-  height: 100%;
-  justify-content: center;
-`;
-
-const DateWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0 1rem;
-  cursor: pointer;
-`;
-
-const ChatOuterWrapper = styled.div`
-  width: 100%;
-  max-width: 106rem;
-  height: 95%;
-  display: flex;
-  padding: 0.94rem 1.25rem;
-  margin: 1.25rem;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 1rem;
-  border-radius: 1.875rem;
-`;
-
-const ChatBodyWrapper = styled.div`
-  width: 100%;
-  height: 90%;
-  overflow-y: auto;
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-  &::-webkit-scrollbar {
-    width: 0;
-    height: 0;
-  }
-`;
-
-const ChatMessageOuterWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const ChatMessageInnerWrapper = styled.div<{ $isMine: boolean }>`
-  display: flex;
-  gap: 1.25rem;
-  justify-content: ${({ $isMine }) => ($isMine ? 'flex-end' : 'flex-start')};
-  align-items: center;
-  width: 100%;
-`;
-
-const ChatImage = styled.img`
-  width: auto;
-  height: 100%;
-  max-height: 2.5rem;
-`;
-
-const ChatMessage = styled.div<{ $isMine: boolean }>`
-  margin: 0.31rem 0;
-  padding: 0.625rem;
-  background-color: ${({ $isMine, theme }) =>
-    $isMine ? theme.chatBalloonColor : 'transparent'};
-  color: ${({ theme }) => theme.textColor};
-  max-width: ${({ $isMine }) => ($isMine ? '60%' : '100%')};
-  word-wrap: break-word;
-  border-radius: ${({ $isMine }) => ($isMine ? '0.5rem' : '0')};
-  text-align: ${({ $isMine }) => ($isMine ? 'right' : 'left')};
-`;
-
-const ChatInputWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-  border-radius: 1.875rem;
-  border: 1px solid #ccc;
-  position: relative;
-
-  /* 포커스 시 스타일 */
-  &:focus-within {
-    border-color: ${({ theme }) =>
-      theme.highlightColor}; /* 포커스 시 하이라이트 색상 */
-    box-shadow: 0 0 5px ${({ theme }) => theme.highlightColor}; /* 포커스 시 외곽선 강조 */
-  }
-`;
-
-const ChatInput = styled.input`
-  flex: 1;
-  padding: 0.94rem 1.25rem 0.94rem 0.1rem;
-  border-radius: 1.875rem;
-  border: none;
-  background-color: transparent;
-  color: ${({ theme }) => theme.textColor};
-  font-size: 1rem;
-  outline: none;
-`;
-
-const DatePickerWrapper = styled.div`
-  display: flex;
-  margin-right: auto;
-  margin-left: 1rem;
-  gap: 1rem;
-  align-items: center;
-`;
-
-const SendIconWrapper = styled.div`
-  margin-left: 0.625rem;
-  margin-right: 0.625rem;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-`;
-
-const SendIcon = styled.svg`
-  fill: ${({ theme }) => theme.sendIconColor};
-`;
-
-const DatePickerText = styled.p`
-  color: ${({ theme }) => theme.profileColor};
-  font-weight: 500;
-  font-size: 1.2rem;
-`;
+import {
+  ChatBodyWrapper,
+  ChatCenter,
+  ChatImage,
+  ChatInput,
+  ChatInputWrapper,
+  ChatMessage,
+  ChatMessageInnerWrapper,
+  ChatMessageOuterWrapper,
+  ChatOuterWrapper,
+  CustomInput,
+  DatePickerText,
+  DatePickerWrapper,
+  DateWrapper,
+  SendIcon,
+  SendIconWrapper,
+} from '@features/Etc/AIChatBot/AIChatBotStyledComponent';
 
 const sendIcon = (
   <SendIcon
@@ -158,25 +47,6 @@ const sendIcon = (
     <path d="M24.456 11.6076L2.45599 0.607565C2.28356 0.521332 2.08988 0.48678 1.89827 0.50807C1.70665 0.52936 1.52528 0.605584 1.37599 0.727565C1.23341 0.847058 1.12699 1.00395 1.0687 1.18061C1.0104 1.35727 1.00254 1.54668 1.04599 1.72756L4.00599 12.4976L1.00599 23.2376C0.965214 23.3886 0.960455 23.5471 0.992092 23.7004C1.02373 23.8536 1.09088 23.9973 1.18815 24.1198C1.28541 24.2424 1.41008 24.3404 1.55212 24.406C1.69416 24.4716 1.84962 24.503 2.00599 24.4976C2.16253 24.4966 2.31667 24.4589 2.45599 24.3876L24.456 13.3876C24.6198 13.3036 24.7573 13.1762 24.8532 13.0191C24.9492 12.8621 25 12.6816 25 12.4976C25 12.3135 24.9492 12.133 24.8532 11.976C24.7573 11.819 24.6198 11.6915 24.456 11.6076ZM3.55599 21.6076L5.76599 13.4976H15.006V11.4976H5.76599L3.55599 3.38756L21.766 12.4976L3.55599 21.6076Z" />
   </SendIcon>
 );
-
-const CustomInput = styled.input`
-  padding: 0.5rem;
-  border-radius: 1rem;
-  border: none;
-  background-color: ${({ theme }) => theme.profileBackgroundColor};
-  color: ${({ theme }) => theme.profileColor};
-  font-size: 1rem;
-  font-weight: 500;
-  width: 100%;
-  outline: none;
-  cursor: pointer;
-  text-align: center;
-
-  &::placeholder {
-    color: ${({ theme }) => theme.profileColor};
-    opacity: 0.7;
-  }
-`;
 
 // ForwardRef component for the custom input
 interface CustomDateInputProps {
@@ -198,11 +68,7 @@ const CustomDateInput = forwardRef<HTMLInputElement, CustomDateInputProps>(
 CustomDateInput.displayName = 'CustomDateInput'; // forwardRef 사용 시 displayName 설정
 
 // Styled DatePicker with props type extensions
-const StyledDatePicker = styled(DatePicker as any)`
-  .react-datepicker-wrapper {
-    width: 100%;
-  }
-`;
+const StyledDatePicker = styled(DatePicker as any)``;
 
 const AIChatBotPage: React.FC = () => {
   const [input, setInput] = useState<string>('');
