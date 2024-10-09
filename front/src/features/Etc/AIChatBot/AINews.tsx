a5582d3a5f203815159c5207a775900135051bc5
이주호 <zxxv1245@naver.com>
2024-10-10 03:24:45 +0900

    chore: #166 경로변경 및 css 분리

--- front/src/features/Etc/AIChatBot/AINews.tsx
+++ front/src/features/Etc/AIChatBot/AINews.tsx
@@ -1,54 +1,13 @@
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-
-const NewsWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  width: 100%;
-  margin-top: 1.25rem;
-  gap: 1.25rem;
-`;
-
-const NewsHeaderText = styled.p`
-  font-weight: 600;
-  font-size: 1.25rem;
-`;
-
-const NewsOuterWrapper = styled.div`
-  display: flex;
-  overflow-x: auto;
-  width: 100%;
-  gap: 1rem;
-`;
-
-const NewsItemWrapper = styled.div`
-  flex: 0 0 auto;
-  width: 18%;
-  height: 12.5rem;
-  padding: 1.25rem 1rem;
-  border: 1px solid #d1d1d1;
-  border-radius: 1rem;
-  background-color: ${({ theme }) => theme.newsBackgroundColor};
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  cursor: pointer;
-`;
-
-const NewsMedia = styled.h3`
-  font-size: 1.1rem;
-  font-weight: 600;
-`;
-
-const NewsTitle = styled.p`
-  font-size: 1.2rem;
-`;
-
-const NewsDate = styled.p`
-  font-size: 0.8rem;
-  margin-bottom: 1rem;
-`;
+import {
+  NewsDate,
+  NewsHeaderText,
+  NewsItemWrapper,
+  NewsMedia,
+  NewsOuterWrapper,
+  NewsTitle,
+  NewsWrapper,
+} from './AIChatBotStyledComponent';
 
 // NewsItem 타입 정의
 interface NewsItem {
