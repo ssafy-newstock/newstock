b30fe53767a3b4fec20c944fcf6b12423a0ee424
이주호 <zxxv1245@naver.com>
2024-10-10 15:34:48 +0900

    refactor: 캔들차트 조회 기간 실시간으로 수정

--- front/src/utils/dateUtils.ts
+++ front/src/utils/dateUtils.ts
@@ -1,9 +1,7 @@
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
 
 export const calculateDates = (days: number) => {
-  const endDate = dayjs('2024-10-01').format('YYYY-MM-DD');
-  const startDate = dayjs(endDate).subtract(days, 'day').format('YYYY-MM-DD');
-  // const endDate = dayjs().format('YYYY-MM-DD');
-  // const startDate = dayjs().subtract(days, 'day').format('YYYY-MM-DD');
+  const endDate = dayjs().format('YYYY-MM-DD');
+  const startDate = dayjs().subtract(days, 'day').format('YYYY-MM-DD');
   return { startDate, endDate };
 };
